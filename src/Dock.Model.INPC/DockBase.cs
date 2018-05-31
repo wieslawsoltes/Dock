c9a7b8f29a69c8fc35db1cdc7a81d97d74844c96
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-05-31 15:33:24 +0200

    Added NavigateAdapter

--- src/Dock.Model.INPC/DockBase.cs
+++ src/Dock.Model.INPC/DockBase.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Dock.Model
 {
@@ -10,14 +9,21 @@ namespace Dock.Model
     /// </summary>
     public abstract class DockBase : ViewBase, IDock
     {
-        private readonly Stack<IView> _back = new Stack<IView>();
-        private readonly Stack<IView> _forward = new Stack<IView>();
+        private NavigateAdapter _navigate;
         private IList<IView> _views;
         private IView _currentView;
         private IView _defaultView;
         private IList<IDockWindow> _windows;
         private string _dock;
         private IDockFactory _factory;
+
+        /// <summary>
+        /// Initializes new instance of the <see cref="DockBase"/> class.
+        /// </summary>
+        public DockBase()
+        {
+            _navigate = new NavigateAdapter(this);
+        }
 
         /// <inheritdoc/>
         public IList<IView> Views
@@ -33,7 +39,6 @@ namespace Dock.Model
             set
             {
                 Update(ref _currentView, value);
-
                 Notify(nameof(CanGoBack));
                 Notify(nameof(CanGoForward));
             }
@@ -47,10 +52,10 @@ namespace Dock.Model
         }
 
         /// <inheritdoc/>
-        public bool CanGoBack => _back.Count > 0;
+        public bool CanGoBack => _navigate.CanGoBack;
 
         /// <inheritdoc/>
-        public bool CanGoForward => _forward.Count > 0;
+        public bool CanGoForward => _navigate.CanGoForward;
 
         /// <inheritdoc/>
         public IList<IDockWindow> Windows
@@ -73,130 +78,22 @@ namespace Dock.Model
             set => Update(ref _factory, value);
         }
 
-
         /// <inheritdoc/>
-        public void GoBack()
+        public virtual void GoBack()
         {
-            if (_back.Count > 0)
-            {
-                var root = _back.Pop();
-                if (CurrentView != null)
-                {
-                    _forward.Push(CurrentView);
-                }
-                NavigateImpl(root, false);
-            }
+            _navigate.GoBack();
         }
 
         /// <inheritdoc/>
-        public void GoForward()
+        public virtual void GoForward()
         {
-            if (_forward.Count > 0)
-            {
-                var root = _forward.Pop();
-                if (CurrentView != null)
-                {
-                    _back.Push(CurrentView);
-                }
-                NavigateImpl(root, false);
-            }
-        }
-
-        /// <summary>
-        /// Resets navigation history.
-        /// </summary>
-        private void ResetNavigation()
-        {
-            if (_back.Count > 0)
-            {
-                _back.Clear();
-            }
-
-            if (_forward.Count > 0)
-            {
-                _forward.Clear();
-            }
-        }
-
-        /// <summary>
-        /// Makes snapshot of most recent dock.
-        /// </summary>
-        /// <param name="view">The view to make snapshot from.</param>
-        private void MakeSnapshot(IView view)
-        {
-            if (_forward.Count > 0)
-                _forward.Clear();
-            _back.Push(view);
-        }
-
-        /// <summary>
-        /// Implementation of the <see cref="IViewsHost.Navigate(object)"/> method.
-        /// </summary>
-        /// <param name="root">An object that contains the content to navigate to.</param>
-        /// <param name="bSnapshot">The lag indicating whether to make snapshot.</param>
-        public void NavigateImpl(object root, bool bSnapshot)
-        {
-            if (root == null)
-            {
-                if (CurrentView is IWindowsHost currentViewWindows)
-                {
-                    currentViewWindows.HideWindows();
-                    CurrentView = null;
-                    ResetNavigation();
-                }
-            }
-            else if (root is IDock dock)
-            {
-                if (CurrentView is IWindowsHost currentViewWindows)
-                {
-                    currentViewWindows.HideWindows();
-                }
-
-                if (dock != null && CurrentView != dock)
-                {
-                    if (CurrentView != null && bSnapshot == true)
-                    {
-                        MakeSnapshot(CurrentView);
-                    }
-
-                    CurrentView = dock;
-                }
-
-                if (dock is IWindowsHost dockWindows)
-                {
-                    dockWindows.ShowWindows();
-                }
-            }
-            else if (root is string id)
-            {
-                var result1 = Views.FirstOrDefault(v => v.Id == id);
-                if (result1 != null)
-                {
-                    NavigateImpl(result1, bSnapshot);
-                }
-                else
-                {
-                    var views = Views.Flatten(v =>
-                    {
-                        if (v is IViewsHost n)
-                        {
-                            return n.Views;
-                        }
-                        return null;
-                    });
-                    var result2 = views.FirstOrDefault(v => v.Id == id);
-                    if (result2 != null)
-                    {
-                        NavigateImpl(result2, bSnapshot);
-                    }
-                }
-            }
+            _navigate.GoForward();
         }
 
         /// <inheritdoc/>
-        public void Navigate(object root)
+        public virtual void Navigate(object root)
         {
-            NavigateImpl(root, true);
+            _navigate.Navigate(root, true);
         }
 
         /// <inheritdoc/>

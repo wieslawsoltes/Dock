f06b867d4ed491d617e06b836a5fbf432f49b443
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-05-31 12:27:52 +0200

    Refactored dock base class

--- src/Dock.Model/DockBase.cs
+++ src/Dock.Model/DockBase.cs
@@ -1,23 +1,15 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Dock.Model
 {
     /// <summary>
     /// Dock base class.
     /// </summary>
-    public abstract class DockBase : ViewBase, IDock
+    public abstract class DockBase : WindowsHostBase, IDock
     {
-        private readonly Stack<IView> _back = new Stack<IView>();
-        private readonly Stack<IView> _forward = new Stack<IView>();
         private string _dock;
         private IDockFactory _factory;
-        private IList<IView> _views;
-        private IView _currentView;
-        private IView _defaultView;
-        private IList<IDockWindow> _windows;
 
         /// <inheritdoc/>
         public string Dock
@@ -33,192 +25,6 @@ namespace Dock.Model
             set => Update(ref _factory, value);
         }
 
-        /// <inheritdoc/>
-        public IList<IView> Views
-        {
-            get => _views;
-            set => Update(ref _views, value);
-        }
-
-        /// <inheritdoc/>
-        public IView CurrentView
-        {
-            get => _currentView;
-            set
-            {
-                Update(ref _currentView, value);
-
-                Notify(nameof(CanGoBack));
-                Notify(nameof(CanGoForward));
-            }
-        }
-
-        /// <inheritdoc/>
-        public IView DefaultView
-        {
-            get => _defaultView;
-            set => Update(ref _defaultView, value);
-        }
-
-        /// <inheritdoc/>
-        public IList<IDockWindow> Windows
-        {
-            get => _windows;
-            set => Update(ref _windows, value);
-        }
-
-        /// <inheritdoc/>
-        public bool CanGoBack => _back.Count > 0;
-
-        /// <inheritdoc/>
-        public bool CanGoForward => _forward.Count > 0;
-
-        /// <inheritdoc/>
-        public void GoBack()
-        {
-            if (_back.Count > 0)
-            {
-                var root = _back.Pop();
-                if (_currentView != null)
-                {
-                    _forward.Push(_currentView);
-                }
-                NavigateImpl(root, false);
-            }
-        }
-
-        /// <inheritdoc/>
-        public void GoForward()
-        {
-            if (_forward.Count > 0)
-            {
-                var root = _forward.Pop();
-                if (_currentView != null)
-                {
-                    _back.Push(_currentView);
-                }
-                NavigateImpl(root, false);
-            }
-        }
-
-        /// <summary>
-        /// Resets navigation history.
-        /// </summary>
-        private void ResetNavigation()
-        {
-            if (_back.Count > 0)
-            {
-                _back.Clear();
-            }
-
-            if (_forward.Count > 0)
-            {
-                _forward.Clear();
-            }
-        }
-
-        /// <summary>
-        /// Makes snapshot of most recent dock.
-        /// </summary>
-        /// <param name="view">The view to make snapshot from.</param>
-        private void MakeSnapshot(IView view)
-        {
-            if (_forward.Count > 0)
-                _forward.Clear();
-            _back.Push(view);
-        }
-
-        /// <summary>
-        /// Implementation of the <see cref="IViewsHost.Navigate(object)"/> method.
-        /// </summary>
-        /// <param name="root">An object that contains the content to navigate to.</param>
-        /// <param name="bSnapshot">The lag indicating whether to make snapshot.</param>
-        public void NavigateImpl(object root, bool bSnapshot)
-        {
-            if (_currentView is IWindowsHost windows)
-            {
-                if (root == null)
-                {
-                    windows.HideWindows();
-                    CurrentView = null;
-                    ResetNavigation();
-                }
-                else if (root is IDock dock)
-                {
-                    if (dock != null && dock != _currentView)
-                    {
-                        windows.HideWindows();
-                    }
-
-                    if (dock != null && _currentView != dock)
-                    {
-                        if (_currentView != null && bSnapshot == true)
-                        {
-                            MakeSnapshot(_currentView);
-                        }
-
-                        CurrentView = dock;
-                    }
-
-                    windows.ShowWindows();
-                }
-                else if (root is string id)
-                {
-                    var result1 = _views.FirstOrDefault(v => v.Id == id);
-                    if (result1 != null)
-                    {
-                        NavigateImpl(result1, bSnapshot);
-                    }
-                    else
-                    {
-                        var views = _views.Flatten(v =>
-                        {
-                            if (v is IViewsHost n)
-                            {
-                                return n.Views;
-                            }
-                            return null;
-                        });
-                        var result2 = views.FirstOrDefault(v => v.Id == id);
-                        if (result2 != null)
-                        {
-                            NavigateImpl(result2, bSnapshot);
-                        }
-                    }
-                }
-            }
-        }
-
-        /// <inheritdoc/>
-        public void Navigate(object root)
-        {
-            NavigateImpl(root, true);
-        }
-
-        /// <inheritdoc/>
-        public virtual void ShowWindows()
-        {
-            if (_windows != null)
-            {
-                foreach (var window in _windows)
-                {
-                    window.Present(false);
-                }
-            }
-        }
-
-        /// <inheritdoc/>
-        public virtual void HideWindows()
-        {
-            if (_windows != null)
-            {
-                foreach (var window in _windows)
-                {
-                    window.Destroy();
-                }
-            }
-        }
-
         /// <summary>
         /// Check whether the <see cref="Dock"/> property has changed from its default value.
         /// </summary>
@@ -230,41 +36,5 @@ namespace Dock.Model
         /// </summary>
         /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
         public virtual bool ShouldSerializeFactory() => false;
-
-        /// <summary>
-        /// Check whether the <see cref="Views"/> property has changed from its default value.
-        /// </summary>
-        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
-        public virtual bool ShouldSerializeViews() => _views != null;
-
-        /// <summary>
-        /// Check whether the <see cref="CurrentView"/> property has changed from its default value.
-        /// </summary>
-        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
-        public virtual bool ShouldSerializeCurrentView() => _currentView != null;
-
-        /// <summary>
-        /// Check whether the <see cref="DefaultView"/> property has changed from its default value.
-        /// </summary>
-        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
-        public virtual bool ShouldSerializeDefaultView() => _defaultView != null;
-
-        /// <summary>
-        /// Check whether the <see cref="Windows"/> property has changed from its default value.
-        /// </summary>
-        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
-        public virtual bool ShouldSerializeWindows() => _windows != null;
-
-        /// <summary>
-        /// Check whether the <see cref="CanGoBack"/> property has changed from its default value.
-        /// </summary>
-        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
-        public virtual bool ShouldSerializeCanGoBack() => false;
-
-        /// <summary>
-        /// Check whether the <see cref="Factory"/> property has changed from its default value.
-        /// </summary>
-        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
-        public virtual bool ShouldSerializeCanGoForward() => false;
     }
 }
---
+++ src/Dock.Model/ViewsHostBase.cs
@@ -0,0 +1,204 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dock.Model
+{
+    /// <summary>
+    /// Views host base class.
+    /// </summary>
+    public abstract class ViewsHostBase : ViewBase, IViewsHost
+    {
+        private readonly Stack<IView> _back = new Stack<IView>();
+        private readonly Stack<IView> _forward = new Stack<IView>();
+        private IList<IView> _views;
+        private IView _currentView;
+        private IView _defaultView;
+
+        /// <inheritdoc/>
+        public IList<IView> Views
+        {
+            get => _views;
+            set => Update(ref _views, value);
+        }
+
+        /// <inheritdoc/>
+        public IView CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                Update(ref _currentView, value);
+
+                Notify(nameof(CanGoBack));
+                Notify(nameof(CanGoForward));
+            }
+        }
+
+        /// <inheritdoc/>
+        public IView DefaultView
+        {
+            get => _defaultView;
+            set => Update(ref _defaultView, value);
+        }
+
+        /// <inheritdoc/>
+        public bool CanGoBack => _back.Count > 0;
+
+        /// <inheritdoc/>
+        public bool CanGoForward => _forward.Count > 0;
+
+        /// <inheritdoc/>
+        public void GoBack()
+        {
+            if (_back.Count > 0)
+            {
+                var root = _back.Pop();
+                if (_currentView != null)
+                {
+                    _forward.Push(_currentView);
+                }
+                NavigateImpl(root, false);
+            }
+        }
+
+        /// <inheritdoc/>
+        public void GoForward()
+        {
+            if (_forward.Count > 0)
+            {
+                var root = _forward.Pop();
+                if (_currentView != null)
+                {
+                    _back.Push(_currentView);
+                }
+                NavigateImpl(root, false);
+            }
+        }
+
+        /// <summary>
+        /// Resets navigation history.
+        /// </summary>
+        private void ResetNavigation()
+        {
+            if (_back.Count > 0)
+            {
+                _back.Clear();
+            }
+
+            if (_forward.Count > 0)
+            {
+                _forward.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Makes snapshot of most recent dock.
+        /// </summary>
+        /// <param name="view">The view to make snapshot from.</param>
+        private void MakeSnapshot(IView view)
+        {
+            if (_forward.Count > 0)
+                _forward.Clear();
+            _back.Push(view);
+        }
+
+        /// <summary>
+        /// Implementation of the <see cref="IViewsHost.Navigate(object)"/> method.
+        /// </summary>
+        /// <param name="root">An object that contains the content to navigate to.</param>
+        /// <param name="bSnapshot">The lag indicating whether to make snapshot.</param>
+        public void NavigateImpl(object root, bool bSnapshot)
+        {
+            if (_currentView is IWindowsHost windows)
+            {
+                if (root == null)
+                {
+                    windows.HideWindows();
+                    CurrentView = null;
+                    ResetNavigation();
+                }
+                else if (root is IDock dock)
+                {
+                    if (dock != null && dock != _currentView)
+                    {
+                        windows.HideWindows();
+                    }
+
+                    if (dock != null && _currentView != dock)
+                    {
+                        if (_currentView != null && bSnapshot == true)
+                        {
+                            MakeSnapshot(_currentView);
+                        }
+
+                        CurrentView = dock;
+                    }
+
+                    windows.ShowWindows();
+                }
+                else if (root is string id)
+                {
+                    var result1 = _views.FirstOrDefault(v => v.Id == id);
+                    if (result1 != null)
+                    {
+                        NavigateImpl(result1, bSnapshot);
+                    }
+                    else
+                    {
+                        var views = _views.Flatten(v =>
+                        {
+                            if (v is IViewsHost n)
+                            {
+                                return n.Views;
+                            }
+                            return null;
+                        });
+                        var result2 = views.FirstOrDefault(v => v.Id == id);
+                        if (result2 != null)
+                        {
+                            NavigateImpl(result2, bSnapshot);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Navigate(object root)
+        {
+            NavigateImpl(root, true);
+        }
+
+        /// <summary>
+        /// Check whether the <see cref="Views"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public virtual bool ShouldSerializeViews() => _views != null;
+
+        /// <summary>
+        /// Check whether the <see cref="CurrentView"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public virtual bool ShouldSerializeCurrentView() => _currentView != null;
+
+        /// <summary>
+        /// Check whether the <see cref="DefaultView"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public virtual bool ShouldSerializeDefaultView() => _defaultView != null;
+
+        /// <summary>
+        /// Check whether the <see cref="CanGoBack"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public virtual bool ShouldSerializeCanGoBack() => false;
+
+        /// <summary>
+        /// Check whether the <see cref="Factory"/> property has changed from its default value.
+        /// </summary>
+        /// <returns>Returns true if the property has changed; otherwise, returns false.</returns>
+        public virtual bool ShouldSerializeCanGoForward() => false;
+    }
+}

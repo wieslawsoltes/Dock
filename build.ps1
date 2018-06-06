8780b454cdfd2fda6899654beff678a24f809bbf
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-06-06 13:30:08 +0000

    Fixed build script

--- build.ps1
+++ build.ps1
@@ -2,44 +2,47 @@
 Param(
     [string]$Configuration = "Release",
     [string]$VersionSuffix = $null,
-    [string]$ApiKey = $null,
-    [string]$ApiUrl = $null,
     [switch]$BuildCore,
     [switch]$TestCore,
     [switch]$PackCore,
     [switch]$BuildSamples,
     [switch]$PublishSamples,
     [switch]$PushNuGet,
+    [switch]$IsNugetRelease,
     [switch]$CopyRedist
 )
 
-if ($VersionSuffix -eq $null) {
+$VersionSuffixParam = $null
+
+if (-Not ($VersionSuffix)) {
     if ($env:APPVEYOR_BUILD_VERSION) {
-        $VersionSuffix = "-build" + $env:APPVEYOR_BUILD_VERSION
+        $VersionSuffixParam = "--version-suffix -build" + $env:APPVEYOR_BUILD_VERSION
+        Write-Host "AppVeyor override VersionSuffix: $VersionSuffix"
     }
-    else {
-        $VersionSuffix = ""
-    }
+} else {
+    $VersionSuffixParam = "--version-suffix $VersionSuffix"
+    Write-Host "VersionSuffix: $VersionSuffix"
 }
 
-if ($PushNuGet -eq $false)
+if (-Not ($PushNuGet))
 {
     if($env:APPVEYOR_REPO_NAME -eq 'wieslawsoltes/Dock' -And $env:APPVEYOR_REPO_BRANCH -eq 'master') {
-        if($env:APPVEYOR_REPO_TAG -eq 'True' -And $env:APPVEYOR_REPO_TAG_NAME) {
-            $ApiKey = $env:NUGET_API_KEY
-            $ApiUrl = $env:NUGET_API_URL
-        }
-        else {
-            $ApiKey = $env:MYGET_API_KEY
-            $ApiUrl = $env:MYGET_API_URL
-        }
         $PushNuGet = $true
+        Write-Host "AppVeyor override PushNuGet: $PushNuGet"
     }
 }
 
-if ($CopyRedist -eq $false) {
+if (-Not ($IsNugetRelease)) {
+    if ($env:APPVEYOR_REPO_TAG -eq 'True' -And $env:APPVEYOR_REPO_TAG_NAME) {
+        $IsNugetRelease = $true
+        Write-Host "AppVeyor override IsNugetRelease: $IsNugetRelease"
+    }
+}
+
+if (-Not ($CopyRedist)) {
     if ($env:APPVEYOR -eq 'True') {
         $CopyRedist = $true
+        Write-Host "AppVeyor override CopyRedist: $CopyRedist"
     }
 }
 
@@ -88,69 +91,126 @@ $SamplesRuntimes = @(
 )
 
 function Execute($cmd) 
-{ 
-    Invoke-Expression $cmd
-    if ($LastExitCode -ne 0) { Exit 1 } 
+{
+    Try {
+        Invoke-Expression $cmd
+        if ($LastExitCode -ne 0) { Exit 1 }
+    }
+    Catch {
+        Write-Host "Invoke Expression failed." -ForegroundColor Red
+        Exit 1
+    }
 }
 
-if ($BuildCore -eq $true) {
+function Invoke-BuildCore
+{
     ForEach ($project in $CoreProjects) {
         ForEach ($framework in $CoreFrameworks) {
-            $cmd = "dotnet build src/$project/$project.csproj -c $Configuration -f $framework --version-suffix $VersionSuffix"
+            Write-Host "Build: $project, $Configuration, $framework"
+            $cmd = "dotnet build src/$project/$project.csproj -c $Configuration -f $framework $VersionSuffixParam"
             Execute $cmd
         }
     }
 }
 
-if ($TestCore -eq $true) {
+function Invoke-TestCore
+{
     ForEach ($project in $TestProjects) {
         ForEach ($framework in $TestFrameworks) {
+            Write-Host "Test: $project, $Configuration, $framework"
             $cmd = "dotnet test tests/$project/$project.csproj -c $Configuration -f $framework"
             Execute $cmd
         }
     }
 }
 
-if ($PackCore -eq $true) {
+function Invoke-PackCore
+{
     ForEach ($project in $CoreProjects) {
-        $cmd = "dotnet pack src/$project/$project.csproj -c $Configuration --version-suffix $VersionSuffix"
+        $cmd = "dotnet pack src/$project/$project.csproj -c $Configuration $VersionSuffixParam"
         Execute $cmd
     }
 }
 
-if ($BuildSamples -eq $true) {
+function Invoke-BuildSamples
+{
     ForEach ($project in $SamplesProjects) {
         ForEach ($framework in $SamplesFrameworks) {
-            $cmd = "dotnet build samples/$project/$project.csproj -c $Configuration -f $framework --version-suffix $VersionSuffix"
+            Write-Host "Build: $project, $Configuration, $framework"
+            $cmd = "dotnet build samples/$project/$project.csproj -c $Configuration -f $framework $VersionSuffixParam"
             Execute $cmd
         }
     }
 }
 
-if ($PublishSamples -eq $true) {
+function Invoke-PublishSamples
+{
     ForEach ($project in $SamplesProjects) {
         ForEach ($framework in $SamplesFrameworks) {
             ForEach ($runtime in $SamplesRuntimes) {
-                $cmd = "dotnet publish samples/$project/$project.csproj -c $Configuration -f $framework -r $runtime --version-suffix $VersionSuffix"
+                Write-Host "Publish: $project, $Configuration, $framework, $runtime"
+                $cmd = "dotnet publish samples/$project/$project.csproj -c $Configuration -f $framework -r $runtime $VersionSuffixParam"
                 Execute $cmd
             }
         }
     }
 }
 
-if($CopyRedist -eq $true) {
-    $RedistPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.14.26405\x64\Microsoft.VC141.CRT"
-    $RedistDest = "$pwd\samples\AvaloniaDemo\bin\AnyCPU\Release"
+function Invoke-CopyRedist
+{
+    $RedistPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.14.26405\x64\"
+    $RedistDest = "$pwd\samples\AvaloniaDemo\bin\AnyCPU\$Configuration"
     $RedistRuntime = "win7-x64"
+    Write-Host "CopyRedist: $RedistDest, $RedistRuntime"
     Copy-Item "$RedistPath\msvcp140.dll" "$RedistDest\netcoreapp2.0\$RedistRuntime\publish"
     Copy-Item "$RedistPath\vcruntime140.dll" "$RedistDest\netcoreapp2.0\$RedistRuntime\publish"
     Copy-Item "$RedistPath\msvcp140.dll" "$RedistDest\netcoreapp2.1\$RedistRuntime\publish"
     Copy-Item "$RedistPath\vcruntime140.dll" "$RedistDest\netcoreapp2.1\$RedistRuntime\publish"
 }
 
-if($PushNuGet -eq $true -And $ApiKey -ne $null -And $ApiUrl -ne $null) {
+function Invoke-PushNuGet
+{
     ForEach ($project in $CoreProjects) {
-        $cmd = "dotnet nuget push src\$project\bin\AnyCPU\$Configuration\*.nupkg -s $ApiUrl -k $ApiKey"
-        Execute $cmd
+        if($IsNugetRelease) {
+            if ($env:NUGET_API_URL -And $env:NUGET_API_KEY) {
+                Write-Host "Push NuGet: $project, $Configuration"
+                $cmd = "dotnet nuget push src/$project/bin/AnyCPU/$Configuration/*.nupkg -s $env:NUGET_API_URL -k $env:NUGET_API_KEY"
+                Execute $cmd
+            }
+        } else {
+            if ($env:MYGET_API_URL -And $env:MYGET_API_KEY) {
+                Write-Host "Push MyGet: $project, $Configuration"
+                $cmd = "dotnet nuget push src/$project/bin/AnyCPU/$Configuration/*.nupkg -s $env:MYGET_API_URL -k $env:MYGET_API_KEY"
+                Execute $cmd
+            }
+        }
     }
+}
+
+if($BuildCore) {
+    Invoke-BuildCore
+}
+
+if($TestCoree) {
+    Invoke-TestCore
+}
+
+if($PackCore) {
+    Invoke-PackCore
+}
+
+if($BuildSamples) {
+    Invoke-BuildSamples
+}
+
+if($PublishSamples) {
+    Invoke-PublishSamples
+}
+
+if($CopyRedist) {
+    Invoke-CopyRedist
+}
+
+if($PushNuGet) {
+    Invoke-PushNuGet
 }

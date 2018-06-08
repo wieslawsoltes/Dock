53024690d97daaadefeba009320d7f7a9832d20c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-06-08 18:36:05 +0200

    Updated behavior

--- src/Dock.Avalonia/SetRootFocusedViewOnFocusBehavior.cs
+++ src/Dock.Avalonia/SetRootFocusedViewOnFocusBehavior.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 using Avalonia.Controls;
-using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Xaml.Interactivity;
 using Dock.Model;
-using System;
 
 namespace Dock.Avalonia
 {
@@ -15,15 +15,6 @@ namespace Dock.Avalonia
     {
         private IDisposable _disposable;
 
-        private IView FindRoot(IView view)
-        {
-            if (view.Parent == null)
-            {
-                return view;
-            }
-            return FindRoot(view.Parent);
-        }
-
         /// <inheritdoc/>
         protected override void OnAttached()
         {
@@ -33,12 +24,15 @@ namespace Dock.Avalonia
             {
                 if (AssociatedObject.DataContext is IDock host && host.Factory is IDockFactory factory)
                 {
-                    if (factory.FindRoot(host.CurrentView) is IViewsHost rootHost)
+                    if (host.CurrentView != null)
                     {
-                        factory.SetFocusedView(rootHost, host.CurrentView);
+                        if (factory.FindRoot(host.CurrentView) is IViewsHost rootHost)
+                        {
+                            factory.SetFocusedView(rootHost, host.CurrentView);
+                        }
                     }
                 }
-            }, global::Avalonia.Interactivity.RoutingStrategies.Tunnel);
+            }, RoutingStrategies.Tunnel);
         }
 
         /// <inheritdoc/>

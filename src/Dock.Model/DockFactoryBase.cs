1e16ba0027297511c9275ddd552f3acdc2ddf072
Dan Walmsley <dan@walms.co.uk>
2018-07-09 16:42:23 +0100

    prototype collapsing code.

--- src/Dock.Model/DockFactoryBase.cs
+++ src/Dock.Model/DockFactoryBase.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Dock.Model.Controls;
 
 namespace Dock.Model
@@ -307,6 +308,21 @@ namespace Dock.Model
                 int index = dock.Views.IndexOf(view);
                 dock.Views.Remove(view);
                 dock.CurrentView = dock.Views.Count > 0 ? dock.Views[index > 0 ? index - 1 : 0] : null;
+
+                if (dock.Views.Count == 0)
+                {
+                    RemoveView(dock);
+
+                    if (dock.Parent is IDock parentDock)
+                    {
+                        var toRemove = parentDock.Views.OfType<ISplitterDock>();
+
+                        foreach (var splitter in toRemove)
+                        {
+                            RemoveView(splitter);
+                        }
+                    }
+                }
             }
         }
 

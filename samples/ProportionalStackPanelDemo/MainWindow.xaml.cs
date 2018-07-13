43df76ea97f4a978c8d86101289e0c18e8a70098
Dan Walmsley <dan@walms.co.uk>
2018-07-13 14:13:29 +0100

    restore demo app.

---
+++ samples/ProportionalStackPanelDemo/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Dock.Avalonia.Controls;
+
+namespace ProportionalStackPanelDemo
+{
+    public class MainWindow : HostWindowBase
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}

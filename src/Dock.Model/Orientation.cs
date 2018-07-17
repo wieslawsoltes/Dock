5d112fa6bc92ddb97ea025135e28ecd6822d164f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-07-17 21:52:14 +0200

    Added Orientation enum

---
+++ src/Dock.Model/Orientation.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Dock.Model
+{
+    /// <summary>
+    /// Defines vertical or horizontal orientation.
+    /// </summary>
+    public enum Orientation
+    {
+        /// <summary>
+        /// Horizontal orientation.
+        /// </summary>
+        Horizontal = 0,
+
+        /// <summary>
+        /// Vertical orientation.
+        /// </summary>
+        Vertical = 1
+    }
+}

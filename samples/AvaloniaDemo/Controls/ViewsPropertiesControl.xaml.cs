d79685f9d6a85c7571a59af4e521e30fef2273ea
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-07-22 14:38:15 +0200

    Split model properties views

---
+++ samples/AvaloniaDemo/Controls/ViewsPropertiesControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemo.Controls
+{
+    public class ViewsPropertiesControl : UserControl
+    {
+        public ViewsPropertiesControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ samples/AvaloniaDemo/Controls/WindowsPropertiesControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemo.Controls
+{
+    public class WindowsPropertiesControl : UserControl
+    {
+        public WindowsPropertiesControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}

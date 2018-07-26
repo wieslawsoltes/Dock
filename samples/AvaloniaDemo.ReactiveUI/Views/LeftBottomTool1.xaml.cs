c42f3147a374f4075446e4d3d9a5f1ad65ffa4bc
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-07-26 21:29:13 +0200

    Added views

---
+++ samples/AvaloniaDemo.ReactiveUI/Views/LeftBottomTool1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemo.ReactiveUI.Views
+{
+    public class LeftBottomTool1 : UserControl
+    {
+        public LeftBottomTool1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}

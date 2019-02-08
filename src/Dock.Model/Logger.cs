69a03513174d94e7dfe7478940a044d242d9db0d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-02-08 23:12:54 +0100

    Update Logger.cs

--- src/Dock.Model/Logger.cs
+++ src/Dock.Model/Logger.cs
@@ -17,7 +17,7 @@ namespace Dock.Model
         [Conditional("DEBUG")]
         public static void Log(string message)
         {
-            Console.WriteLine(message);
+            Debug.WriteLine(message);
         }
     }
 }

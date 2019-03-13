44aadcdf6c69a58915a9d4f1c77726d862164b80
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-03-13 17:49:46 +0100

    Added DragAndDrop view

---
+++ samples/AvaloniaDemo/Views/DragAndDrop.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Dock.Model;
+
+namespace AvaloniaDemo.Views
+{
+    public class DragAndDrop : UserControl
+    {
+        private TextBlock _DropState;
+        private TextBlock _DragState;
+        private Border _DragMe;
+        private int DragCount = 0;
+
+        public DragAndDrop()
+        {
+            this.InitializeComponent();
+            _DragMe.PointerPressed += DoDrag;
+            AddHandler(DragDrop.DropEvent, Drop);
+            AddHandler(DragDrop.DragOverEvent, DragOver);
+        }
+
+        private async void DoDrag(object sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            Logger.Log($"[DragAndDrop] DoDrag");
+            DataObject dragData = new DataObject();
+            dragData.Set(DataFormats.Text, $"You have dragged text {++DragCount} times");
+            var result = await DragDrop.DoDragDrop(dragData, DragDropEffects.Copy);
+            switch (result)
+            {
+                case DragDropEffects.Copy:
+                    _DragState.Text = "The text was copied";
+                    break;
+                case DragDropEffects.Link:
+                    _DragState.Text = "The text was linked";
+                    break;
+                case DragDropEffects.None:
+                    _DragState.Text = "The drag operation was canceled";
+                    break;
+            }
+        }
+
+        private void DragOver(object sender, DragEventArgs e)
+        {
+            Logger.Log($"[DragAndDrop] DragOver");
+            e.DragEffects = e.DragEffects & (DragDropEffects.Copy | DragDropEffects.Link);
+            if (!e.Data.Contains(DataFormats.Text) && !e.Data.Contains(DataFormats.FileNames))
+            {
+                e.DragEffects = DragDropEffects.None;
+                Logger.Log($"[DragAndDrop] DragOver Files No");
+            }
+            else
+            {
+                Logger.Log($"[DragAndDrop] DragOver Files Yes");
+            }
+        }
+
+        private void Drop(object sender, DragEventArgs e)
+        {
+            Logger.Log($"[DragAndDrop] Drop");
+            if (e.Data.Contains(DataFormats.Text))
+                _DropState.Text = e.Data.GetText();
+            else if (e.Data.Contains(DataFormats.FileNames))
+                _DropState.Text = string.Join(Environment.NewLine, e.Data.GetFileNames());
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            _DropState = this.Find<TextBlock>("DropState");
+            _DragState = this.Find<TextBlock>("DragState");
+            _DragMe = this.Find<Border>("DragMe");
+        }
+    }
+}

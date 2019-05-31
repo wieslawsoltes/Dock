a1e3f772f7b3bc448ac6b8cddf970d26802ade6b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-05-31 22:43:51 +0200

    Update ProportionalStackPanelTests.cs

--- tests/Dock.Avalonia.UnitTests/Controls/ProportionalStackPanelTests.cs
+++ tests/Dock.Avalonia.UnitTests/Controls/ProportionalStackPanelTests.cs
@@ -1,8 +1,12 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
 using Avalonia.Controls.Shapes;
+using Avalonia.Controls.Templates;
+using Avalonia.Markup.Xaml.Templates;
 using Avalonia.Media;
 using Dock.Avalonia.Controls;
 using Xunit;
@@ -12,7 +16,7 @@ namespace Dock.Avalonia.UnitTests.Controls
     public class ProportionalStackPanelTests
     {
         [Fact]
-        public void Lays_Out_Children_Horizontal()
+        public void Lays_Out_Children_Default()
         {
             var target = new ProportionalStackPanel()
             {
@@ -100,25 +104,42 @@ namespace Dock.Avalonia.UnitTests.Controls
         }
 
         [Fact]
-        public void Lays_Out_Children_Vertical()
+        public void Lays_Out_Children_ItemsControl()
         {
-            var target = new ProportionalStackPanel()
+            var target1 = new ItemsControl()
             {
                 Width = 1000,
                 Height = 500,
-                Orientation = Orientation.Vertical,
-                Children =
+                ItemsPanel = new ItemsPanelTemplate()
                 {
-                    new ProportionalStackPanel()
+                    Content = new ProportionalStackPanel()
                     {
-                        Children =
+                        Orientation = Orientation.Horizontal
+                    }
+                },
+                Items = new List<IControl>()
+                {
+                    new Rectangle()
+                    {
+                        Fill = Brushes.Green
+                    },
+                    new ProportionalStackPanelSplitter(),
+                    new Rectangle()
+                    {
+                        Fill = Brushes.Blue
+                    },
+                    new ProportionalStackPanelSplitter(),
+                    new ItemsControl()
+                    {
+                        ItemsPanel = new ItemsPanelTemplate()
                         {
-                            new Rectangle()
+                            Content = new ProportionalStackPanel()
                             {
-                                Fill = Brushes.Red,
-                                [ProportionalStackPanelSplitter.ProportionProperty] = 0.5
-                            },
-                            new ProportionalStackPanelSplitter(),
+                                Orientation = Orientation.Vertical,
+                            }
+                        },
+                        Items = new List<IControl>()
+                        {
                             new Rectangle()
                             {
                                 Fill = Brushes.Green
@@ -126,65 +147,37 @@ namespace Dock.Avalonia.UnitTests.Controls
                             new ProportionalStackPanelSplitter(),
                             new Rectangle()
                             {
-                                Fill=Brushes.Blue
-                            }
-                        }
-                    },
-                    new ProportionalStackPanelSplitter(),
-                    new ProportionalStackPanel()
-                    {
-                        Children =
-                        {
-                            new Rectangle()
-                            {
-                                Fill = Brushes.Blue,
+                                Fill = Brushes.Blue
                             },
-                            new ProportionalStackPanelSplitter(),
                             new Rectangle()
                             {
                                 Fill = Brushes.Red
-                            },
-                            new ProportionalStackPanelSplitter(),
-                            new Rectangle()
-                            {
-                                Fill=Brushes.Green
                             }
                         }
-                    },
-                    new ProportionalStackPanelSplitter(),
-                    new ProportionalStackPanel()
-                    {
-                        Children =
-                        {
-                            new Rectangle()
-                            {
-                                Fill = Brushes.Green,
-                            },
-                            new ProportionalStackPanelSplitter(),
-                            new Rectangle()
-                            {
-                                Fill = Brushes.Blue
-                            },
-                            new ProportionalStackPanelSplitter(),
-                            new Rectangle()
-                            {
-                                Fill=Brushes.Red,
-                                [ProportionalStackPanelSplitter.ProportionProperty] = 0.5
-                            }
-                        }
-                    },
+                    }
                 }
             };
 
-            target.Measure(Size.Infinity);
-            target.Arrange(new Rect(target.DesiredSize));
+            target1.ApplyTemplate();
+            target1.Measure(Size.Infinity);
+            target1.Arrange(new Rect(target1.DesiredSize));
 
-            Assert.Equal(new Size(1000, 500), target.Bounds.Size);
-            Assert.Equal(new Rect(0, 0, 1000, 164), target.Children[0].Bounds);
-            Assert.Equal(new Rect(0, 164, 1000, 0), target.Children[1].Bounds);
-            Assert.Equal(new Rect(0, 164, 1000, 164), target.Children[2].Bounds);
-            Assert.Equal(new Rect(0, 328, 1000, 0), target.Children[3].Bounds);
-            Assert.Equal(new Rect(0, 328, 1000, 164), target.Children[4].Bounds);
+            var items1 = target1.Items as List<IControl>;
+            var target2 = items1[4] as ItemsControl;
+            var items2 = target2.Items as List<IControl>;
+
+            Assert.Equal(new Size(1000, 500), target1.Bounds.Size);
+            Assert.Equal(new Rect(0, 0, 0, 0), items1[0].Bounds);
+            Assert.Equal(new Rect(0, 0, 0, 0), items1[1].Bounds);
+            Assert.Equal(new Rect(0, 0, 0, 0), items1[2].Bounds);
+            Assert.Equal(new Rect(0, 0, 0, 0), items1[3].Bounds);
+            Assert.Equal(new Rect(0, 0, 0, 0), items1[4].Bounds);
+
+            Assert.Equal(new Size(0, 0), target2.Bounds.Size);
+            Assert.Equal(new Rect(0, 0, 0, 0), items2[0].Bounds);
+            Assert.Equal(new Rect(0, 0, 0, 0), items2[1].Bounds);
+            Assert.Equal(new Rect(0, 0, 0, 0), items2[2].Bounds);
+            Assert.Equal(new Rect(0, 0, 0, 0), items2[3].Bounds);
         }
     }
 }

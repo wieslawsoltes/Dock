70e1e1765c73f8d6a10c9c9535141c48c33387e3
Dan Walmsley <dan@walms.co.uk>
2019-08-05 21:45:24 +0100

    start updating avalonia

--- src/Dock.Avalonia/DragDrop/DragBehavior.cs
+++ src/Dock.Avalonia/DragDrop/DragBehavior.cs
@@ -85,7 +85,7 @@ namespace Dock.Avalonia
 
         private async void DoDrag(object sender, PointerPressedEventArgs e)
         {
-            if (e.MouseButton == MouseButton.Left && GetIsEnabled(AssociatedObject))
+            if (e.InputModifiers.HasFlag(InputModifiers.LeftMouseButton) && GetIsEnabled(AssociatedObject))
             {
                 Handler?.BeforeDragDrop(sender, e, Context);
 

636d7d49166018f9019dd1643d4e7b9dd3c368b2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-08-06 18:22:56 +0200

    Added Dock.Model.Avalonia.UnitTests project

---
+++ tests/Dock.Model.Avalonia.UnitTests/Controls/DocumentTabTests.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Dock.Model.Controls;
+using Xunit;
+
+namespace Dock.Model.Avalonia.UnitTests.Controls
+{
+    public class DocumentTabTests
+    {
+        [Fact]
+        public void DocumentTab_Ctor()
+        {
+            var actual = new DocumentTab();
+            Assert.NotNull(actual);
+        }
+    }
+}

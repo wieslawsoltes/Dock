7e41423db2fe9a8827d2927a092d6e319f6efe78
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2019-08-07 13:52:40 +0200

    Update MetroWindow.cs

--- src/Dock.Avalonia/Controls/MetroWindow.cs
+++ src/Dock.Avalonia/Controls/MetroWindow.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Styling;
-using System;
 
 namespace Dock.Avalonia.Controls
 {

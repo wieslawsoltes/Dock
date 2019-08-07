0e123ecaf736bab1e9222478bc02f448c827a1b1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2019-08-07 08:41:32 +0200

    Update SplitterDock.cs

--- src/Dock.Model.Avalonia/Controls/SplitterDock.cs
+++ src/Dock.Model.Avalonia/Controls/SplitterDock.cs
@@ -10,5 +10,8 @@ namespace Dock.Model.Controls
     [DataContract(IsReference = true)]
     public class SplitterDock : DockBase, ISplitterDock
     {
+        static SplitterDock()
+        {
+        }
     }
 }

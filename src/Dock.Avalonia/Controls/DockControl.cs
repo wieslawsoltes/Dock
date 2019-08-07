0a1e02daa15bbea8d13ab303511bdf05bb8907a9
wieslawsoltes <wieslaw.soltes@gmail.com>
2019-08-07 09:24:25 +0200

    Refactored dock control

---
+++ src/Dock.Avalonia/Controls/DockControl.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Dock.Model;
+
+namespace Dock.Avalonia.Controls
+{
+    /// <summary>
+    /// Interaction logic for <see cref="DockControl"/> xaml.
+    /// </summary>
+    public class DockControl : TemplatedControl
+    {
+        /// <summary>
+        /// Defines the <see cref="Layout"/> property.
+        /// </summary>
+        public static readonly StyledProperty<IDock> LayoutProperty =
+            AvaloniaProperty.Register<DockPanel, IDock>(nameof(Layout));
+
+        /// <summary>
+        /// Gets or sets the dock layout.
+        /// </summary>
+        /// <value>The layout.</value>
+        public IDock Layout
+        {
+            get { return GetValue(LayoutProperty); }
+            set { SetValue(LayoutProperty, value); }
+        }
+    }
+}

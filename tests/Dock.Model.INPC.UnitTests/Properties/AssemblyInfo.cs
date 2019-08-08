09b9ef7d3b4395998faf37560d08fbf5279424e0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-08-08 22:31:50 +0200

    Don't run tests in parallel

---
+++ tests/Dock.Model.INPC.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Reflection;
+using Xunit;
+
+[assembly: AssemblyTitle("Dock.Model.INPC.UnitTests")]
+
+// Don't run tests in parallel.
+[assembly: CollectionBehavior(DisableTestParallelization = true)]

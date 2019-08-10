92895a6098e39f2934565b1fc18d71293c4928b0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-08-10 13:49:05 +0200

    Updated sample app

--- samples/AvaloniaDemo/App.xaml.cs
+++ samples/AvaloniaDemo/App.xaml.cs
@@ -1,5 +1,13 @@
 ﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using AvaloniaDemo.Factories;
+using AvaloniaDemo.Model;
+using AvaloniaDemo.Serializer;
+using AvaloniaDemo.ViewModels;
+using Dock.Model;
+using Dock.Model.Controls;
+using ReactiveUI.Legacy;
 
 namespace AvaloniaDemo
 {
@@ -8,6 +16,55 @@ namespace AvaloniaDemo
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                var serializer = new DockJsonSerializer(typeof(ReactiveList<>));
+#pragma warning restore CS0618 // Type or member is obsolete
+                var vm = new MainWindowViewModel();
+                var factory = new DemoDockFactory(new DemoData());
+                IDock layout = null;
+
+                string path = serializer.GetBasePath("Layout.json");
+                if (serializer.Exists(path))
+                {
+                    layout = serializer.Load<RootDock>(path);
+                }
+
+                var window = new MainWindow
+                {
+                    DataContext = vm
+                };
+
+                vm.Factory = factory;
+                vm.Layout = layout ?? vm.Factory.CreateLayout();
+                vm.Factory.InitLayout(vm.Layout);
+
+                window.Closing += (sender, e) =>
+                {
+                    // TODO: Save main window position, size and state.
+                };
+
+                desktopLifetime.MainWindow = window;
+
+                desktopLifetime.Exit += (sennder, e) =>
+                {
+                    if (vm.Layout is IDock dock)
+                    {
+                        dock.Close();
+                    }
+                    serializer.Save(path, vm.Layout);
+                };
+            }
+            //else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewLifetime)
+            //{
+            //    singleViewLifetime.MainView = new MainView();
+            //}
+            base.OnFrameworkInitializationCompleted();
         }
     }
 }
--- samples/AvaloniaDemo/Program.cs
+++ samples/AvaloniaDemo/Program.cs
@@ -1,63 +1,15 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Logging.Serilog;
-using AvaloniaDemo.Factories;
-using AvaloniaDemo.Model;
-using AvaloniaDemo.Serializer;
-using AvaloniaDemo.ViewModels;
-using Dock.Model;
-using Dock.Model.Controls;
 
 namespace AvaloniaDemo
 {
     internal class Program
     {
-        private static void Print(Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-            Console.WriteLine(ex.StackTrace);
-            if (ex.InnerException != null)
-            {
-                Print(ex.InnerException);
-            }
-        }
-
         [STAThread]
         private static void Main(string[] args)
         {
-            try
-            {
-                var serializer = new DockJsonSerializer(typeof(ObservableCollection<>));
-                var vm = new MainWindowViewModel();
-                var factory = new DemoDockFactory(new DemoData());
-                IDock layout = null;
-
-                string path = serializer.GetBasePath("Layout.json");
-                if (serializer.Exists(path))
-                {
-                    layout = serializer.Load<RootDock>(path);
-                }
-
-                BuildAvaloniaApp().Start<MainWindow>(() =>
-                {
-                    vm.Factory = factory;
-                    vm.Layout = layout ?? vm.Factory.CreateLayout();
-                    vm.Factory.InitLayout(vm.Layout);
-                    return vm;
-                });
-
-                if (vm.Layout is IDock dock)
-                {
-                    dock.Close();
-                }
-
-                serializer.Save(path, vm.Layout);
-            }
-            catch (Exception ex)
-            {
-                Print(ex);
-            }
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
         }
 
         public static AppBuilder BuildAvaloniaApp()

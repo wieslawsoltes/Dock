af5459a7fa8786c1422751a70f4bce3ca1f6c9d8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-08-15 09:02:08 +0200

    Debug pointer events

--- src/Dock.Avalonia/Controls/DockControl.cs
+++ src/Dock.Avalonia/Controls/DockControl.cs
@@ -1,8 +1,14 @@
 // Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Diagnostics;
+using System.Linq;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 using Avalonia.Metadata;
+using Avalonia.Rendering;
+using Avalonia.VisualTree;
 using Dock.Model;
 
 namespace Dock.Avalonia.Controls
@@ -27,6 +33,80 @@ namespace Dock.Avalonia.Controls
         {
             get { return GetValue(LayoutProperty); }
             set { SetValue(LayoutProperty, value); }
+        }
+
+        [Conditional("DEBUG")]
+        private void Print(PointerEventArgs e, string name)
+        {
+            var point = e.GetPosition(this);
+
+            if (VisualRoot is IRenderRoot root)
+            {
+                var visuals = root.GetVisualsAt(point, x => (!(x is AdornerLayer) && x.IsVisible)).ToList();
+                if (visuals.Count > 0)
+                {
+                    Debug.WriteLine($"{name} : {root.GetType().Name} : {point}");
+                    foreach (var visual in visuals)
+                    {
+                        if (visual is IControl control)
+                        {
+                            Debug.WriteLine($"- {control.GetType().Name} : {control.DataContext?.GetType().Name}");
+                        }
+                        else
+                        {
+                            Debug.WriteLine($"- {visual.GetType().Name}");
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+            Print(e, nameof(OnPointerPressed));
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPointerReleased(PointerReleasedEventArgs e)
+        {
+            base.OnPointerReleased(e);
+            Print(e, nameof(OnPointerReleased));
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPointerMoved(PointerEventArgs e)
+        {
+            base.OnPointerMoved(e);
+            Print(e, nameof(OnPointerMoved));
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPointerEnter(PointerEventArgs e)
+        {
+            base.OnPointerEnter(e);
+            Print(e, nameof(OnPointerEnter));
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPointerLeave(PointerEventArgs e)
+        {
+            base.OnPointerLeave(e);
+            Print(e, nameof(OnPointerLeave));
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPointerCaptureLost(PointerCaptureLostEventArgs e)
+        {
+            base.OnPointerCaptureLost(e);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
+        {
+            base.OnPointerWheelChanged(e);
+            Print(e, nameof(OnPointerWheelChanged));
         }
     }
 }

9b14e7277e3d7678b0620871092754c63faff324
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-08-21 20:02:34 +0200

    Update DashboardViewModel.cs

--- samples/AvaloniaDemo/ViewModels/Views/DashboardViewModel.cs
+++ samples/AvaloniaDemo/ViewModels/Views/DashboardViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Dock.Model;
+using Dock.Model.Controls;
 
 namespace AvaloniaDemo.ViewModels.Views
 {
@@ -7,7 +8,7 @@ namespace AvaloniaDemo.ViewModels.Views
     {
         public override IDockable Clone()
         {
-            var dashboardViewModel = new HomeViewModel();
+            var dashboardViewModel = new DashboardViewModel();
 
             CloneHelper.CloneDockProperties(this, dashboardViewModel);
 

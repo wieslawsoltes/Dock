c27b8c29d3fbc361b22994b4bbb3a3595e9793e1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-08-28 22:11:57 +0200

    Add find and replace view contents

--- samples/Notepad/ViewModels/Tools/FindViewModel.cs
+++ samples/Notepad/ViewModels/Tools/FindViewModel.cs
@@ -1,8 +1,34 @@
-﻿using Dock.Model.Controls;
+﻿using System.Runtime.Serialization;
+using Dock.Model;
+using Dock.Model.Controls;
+using Notepad.ViewModels.Documents;
+using ReactiveUI;
 
 namespace Notepad.ViewModels.Tools
 {
     public class FindViewModel : Tool
     {
+        private string _find;
+
+        [DataMember(IsRequired = false, EmitDefaultValue = false)]
+        public string Find
+        {
+            get => _find;
+            set => this.RaiseAndSetIfChanged(ref _find, value);
+        }
+
+        public void FindNext()
+        {
+            if (this.Context is IRootDock root)
+            {
+                if (root.Factory.FindDockable(root, (d) => d.Id == "Files") is IDock files)
+                {
+                    if (files.ActiveDockable is FileViewModel fileViewModel)
+                    {
+                        // TODO: 
+                    }
+                }
+            }
+        }
     }
 }
--- samples/Notepad/ViewModels/Tools/ReplaceViewModel.cs
+++ samples/Notepad/ViewModels/Tools/ReplaceViewModel.cs
@@ -1,8 +1,42 @@
-﻿using Dock.Model.Controls;
+﻿using System.Runtime.Serialization;
+using Dock.Model;
+using Dock.Model.Controls;
+using Notepad.ViewModels.Documents;
+using ReactiveUI;
 
 namespace Notepad.ViewModels.Tools
 {
     public class ReplaceViewModel : Tool
     {
+        private string _find;
+        private string _replace;
+
+        [DataMember(IsRequired = false, EmitDefaultValue = false)]
+        public string Find
+        {
+            get => _find;
+            set => this.RaiseAndSetIfChanged(ref _find, value);
+        }
+
+        [DataMember(IsRequired = false, EmitDefaultValue = false)]
+        public string Replace
+        {
+            get => _replace;
+            set => this.RaiseAndSetIfChanged(ref _replace, value);
+        }
+
+        public void ReplaceNext()
+        {
+            if (this.Context is IRootDock root)
+            {
+                if (root.Factory.FindDockable(root, (d) => d.Id == "Files") is IDock files)
+                {
+                    if (files.ActiveDockable is FileViewModel fileViewModel)
+                    {
+                        // TODO: 
+                    }
+                }
+            }
+        }
     }
 }

7c58a6efcd2b13afbf45d649f1066f9dfe0b24d5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-09-18 21:14:15 +0200

    Added window stubs

---
+++ samples/Notepad/Views/Layouts/ManageWindowLayoutsWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Notepad.Views.Layouts
+{
+    public class ManageWindowLayoutsWindow : Window
+    {
+        public ManageWindowLayoutsWindow()
+        {
+            this.InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}

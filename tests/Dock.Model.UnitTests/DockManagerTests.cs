51e600575db32d1e78b7b37b5eeab109a19f00eb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-09-25 00:04:55 +0200

    Update DockManagerTests.cs

--- tests/Dock.Model.UnitTests/DockManagerTests.cs
+++ tests/Dock.Model.UnitTests/DockManagerTests.cs
@@ -17,7 +17,9 @@ namespace Dock.Model.UnitTests
         public void Validate_sourceDockable_Null()
         {
             var manager = new DockManager();
+#pragma warning disable CS8625
             var actual = manager.ValidateDockable(null, null, DragAction.Move, DockOperation.Fill, false);
+#pragma warning restore CS8625
             Assert.False(actual);
         }
     }

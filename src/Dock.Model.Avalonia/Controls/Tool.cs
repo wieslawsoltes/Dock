748c9fdc4906cb39d9c2a796f987deed01d03316
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-08-18 22:30:16 +0200

    Update Tool.cs

--- src/Dock.Model.Avalonia/Controls/Tool.cs
+++ src/Dock.Model.Avalonia/Controls/Tool.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.Serialization;
+using Avalonia;
+using Avalonia.Metadata;
 
 namespace Dock.Model.Controls
 {
@@ -6,8 +8,24 @@ namespace Dock.Model.Controls
     /// Tool.
     /// </summary>
     [DataContract(IsReference = true)]
-    public abstract class Tool : DockableBase, ITool, IDocument
+    public class Tool : DockableBase, ITool, IDocument
     {
+        /// <summary>
+        /// Defines the <see cref="Content"/> property.
+        /// </summary>
+        public static readonly StyledProperty<object> ContentProperty =
+            AvaloniaProperty.Register<Tool, object>(nameof(Content));
+
+        /// <summary>
+        /// Gets or sets the content to display.
+        /// </summary>
+        [Content]
+        public object Content
+        {
+            get { return GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
         /// <summary>
         /// Initializes new instance of the <see cref="Tool"/> class.
         /// </summary>

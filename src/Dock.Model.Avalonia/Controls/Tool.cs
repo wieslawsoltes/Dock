3cf75b86fac0addf87a1052ec00ac75fb09ab08f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-08-18 22:34:46 +0200

    Revert "Update Tool.cs"
    This reverts commit 748c9fdc4906cb39d9c2a796f987deed01d03316.

--- src/Dock.Model.Avalonia/Controls/Tool.cs
+++ src/Dock.Model.Avalonia/Controls/Tool.cs
@@ -1,6 +1,4 @@
 ﻿using System.Runtime.Serialization;
-using Avalonia;
-using Avalonia.Metadata;
 
 namespace Dock.Model.Controls
 {
@@ -8,24 +6,8 @@ namespace Dock.Model.Controls
     /// Tool.
     /// </summary>
     [DataContract(IsReference = true)]
-    public class Tool : DockableBase, ITool, IDocument
+    public abstract class Tool : DockableBase, ITool, IDocument
     {
-        /// <summary>
-        /// Defines the <see cref="Content"/> property.
-        /// </summary>
-        public static readonly StyledProperty<object> ContentProperty =
-            AvaloniaProperty.Register<Tool, object>(nameof(Content));
-
-        /// <summary>
-        /// Gets or sets the content to display.
-        /// </summary>
-        [Content]
-        public object Content
-        {
-            get { return GetValue(ContentProperty); }
-            set { SetValue(ContentProperty, value); }
-        }
-
         /// <summary>
         /// Initializes new instance of the <see cref="Tool"/> class.
         /// </summary>

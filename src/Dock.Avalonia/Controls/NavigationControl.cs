4be7f65d1f90f5ef86188be486839c5827339c4f
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-08-18 04:59:46 +0000

    Update headers

--- src/Dock.Avalonia/Controls/NavigationControl.cs
+++ src/Dock.Avalonia/Controls/NavigationControl.cs
@@ -1,5 +1,3 @@
-// Copyright (c) Wiesław Šoltés. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Avalonia.Controls.Primitives;
 
 namespace Dock.Avalonia.Controls
--- tests/Dock.Avalonia.UnitTests/Controls/DocumentControlTests.cs
+++ tests/Dock.Avalonia.UnitTests/Controls/DocumentControlTests.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using Dock.Avalonia.Controls;
+﻿using Dock.Avalonia.Controls;
 using Xunit;
 
 namespace Dock.Avalonia.UnitTests.Controls

06edadccec8a7949c4a26c67961ca0ca353b019f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-17 23:10:20 +0200

    Add ToolPinnedControl

---
+++ src/Dock.Avalonia/Controls/ToolPinnedControl.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls.Primitives;
+
+namespace Dock.Avalonia.Controls
+{
+    /// <summary>
+    /// Interaction logic for <see cref="ToolPinnedControl"/> xaml.
+    /// </summary>
+    public class ToolPinnedControl : TemplatedControl
+    {
+    }
+}

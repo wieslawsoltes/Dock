3dd11f13c0a1a5ee9306c057c0c1d285716e9568
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-25 13:08:16 +0200

    Fix HostWindow drag

--- src/Dock.Avalonia/Controls/HostWindow.axaml.cs
+++ src/Dock.Avalonia/Controls/HostWindow.axaml.cs
@@ -3,10 +3,8 @@ using System.ComponentModel;
 using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Chrome;
 using Avalonia.Controls.Metadata;
 using Avalonia.Input;
-using Avalonia.Interactivity;
 using Avalonia.Styling;
 using Dock.Avalonia.Internal;
 using Dock.Model;
@@ -20,16 +18,10 @@ namespace Dock.Avalonia.Controls
     [PseudoClasses(":toolwindow")]
     public class HostWindow : Window, IStyleable, IHostWindow
     {
-        /// <summary>
-        /// Use custom drag instead of BeginMoveDrag.
-        /// </summary>
-        public static bool s_useCustomDrag = true;
-
         private readonly DockManager _dockManager;
         private readonly HostWindowState _hostWindowState;
         private Control? _chromeGrip;
         private bool _mouseDown;
-        private Point _startPoint;
 
         /// <summary>
         /// Define <see cref="IsToolWindow"/> property.
@@ -65,11 +57,6 @@ namespace Dock.Avalonia.Controls
         /// </summary>
         public HostWindow()
         {
-            AddHandler(PointerPressedEvent, Pressed, RoutingStrategies.Direct | RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
-            AddHandler(PointerReleasedEvent, Released, RoutingStrategies.Direct | RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
-            AddHandler(PointerMovedEvent, Moved, RoutingStrategies.Direct | RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
-            AddHandler(PointerCaptureLostEvent, CaptureLost, RoutingStrategies.Direct | RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
-
             PositionChanged += HostWindow_PositionChanged;
             LayoutUpdated += HostWindow_LayoutUpdated;
 
@@ -79,6 +66,82 @@ namespace Dock.Avalonia.Controls
             this.AttachDevTools();
 #endif
             UpdatePseudoClasses(IsToolWindow);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            if (_chromeGrip is { } && _chromeGrip.IsPointerOver)
+            {
+                if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+                {
+                    _mouseDown = true;
+                    _hostWindowState.Process(e.GetPosition(this), EventType.Pressed);
+
+                    BeginMoveDrag(e);
+
+                    _hostWindowState.Process(e.GetPosition(this), EventType.Released);
+                    _mouseDown = false;
+                }
+            }
+        }
+
+        private void HostWindow_PositionChanged(object? sender, PixelPointEventArgs e)
+        {
+            if (Window is { } && IsTracked)
+            {
+                Window.Save();
+
+                if (_chromeGrip is { } && _chromeGrip.IsPointerOver && _mouseDown)
+                {
+                    _hostWindowState.Process(Position.ToPoint(1.0), EventType.Moved);
+                }
+            }
+        }
+
+        private void HostWindow_LayoutUpdated(object? sender, EventArgs e)
+        {
+            if (Window is { } && IsTracked)
+            {
+                Window.Save();
+            }
+        }
+
+        /// <summary>
+        /// Attaches grip to chrome.
+        /// </summary>
+        /// <param name="chromeControl">The chrome control.</param>
+        public void AttachGrip(ToolChromeControl chromeControl)
+        {
+            if (chromeControl.CloseButton is not null)
+            {
+                Observable.FromEventPattern(chromeControl.CloseButton, nameof(Button.Click)).Subscribe(_ =>
+                {
+                    Exit();
+                });
+            }
+
+            _chromeGrip = chromeControl.Grip;
+            ((IPseudoClasses)chromeControl.Classes).Add(":floating");
+            IsToolWindow = true;
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == IsToolWindowProperty)
+            {
+                UpdatePseudoClasses(change.NewValue.GetValueOrDefault<bool>());
+            }
+        }
+
+        private void UpdatePseudoClasses(bool isToolWindow)
+        {
+            PseudoClasses.Set(":toolwindow", isToolWindow);
         }
 
         /// <inheritdoc/>
@@ -132,126 +195,6 @@ namespace Dock.Avalonia.Controls
                         root.Close.Execute(null);
                     }
                 }
-            }
-        }
-
-        private void HostWindow_PositionChanged(object? sender, PixelPointEventArgs e)
-        {
-            if (Window is { } && IsTracked)
-            {
-                Window.Save();
-            }
-
-            _hostWindowState.Process(Position.ToPoint(1.0), EventType.Moved);
-        }
-
-        private void HostWindow_LayoutUpdated(object? sender, EventArgs e)
-        {
-            if (Window is { } && IsTracked)
-            {
-                Window.Save();
-            }
-        }
-
-        private void Pressed(object? sender, PointerPressedEventArgs e)
-        {
-            _hostWindowState.Process(e.GetPosition(this), EventType.Pressed);
-        }
-
-        private void Released(object? sender, PointerReleasedEventArgs e)
-        {
-            _hostWindowState.Process(e.GetPosition(this), EventType.Released);
-        }
-
-        private void Moved(object? sender, PointerEventArgs e)
-        {
-            // Using PositionChanged event instead of PointerMoved event.
-        }
-
-        private void CaptureLost(object? sender, PointerCaptureLostEventArgs e)
-        {
-            _hostWindowState.Process(new Point(), EventType.CaptureLost);
-        }
-
-        /// <summary>
-        /// Attaches grip to chrome.
-        /// </summary>
-        /// <param name="chromeControl">The chrome control.</param>
-        public void AttachGrip(ToolChromeControl chromeControl)
-        {
-            if (chromeControl.CloseButton is not null)
-            {
-                Observable.FromEventPattern(chromeControl.CloseButton, nameof(Button.Click)).Subscribe(_ =>
-                {
-                    Exit();
-                });
-            }
-
-            _chromeGrip = chromeControl.Grip;
-            ((IPseudoClasses)chromeControl.Classes).Add(":floating");
-            IsToolWindow = true;
-        }
-
-        /// <inheritdoc/>
-        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
-        {
-            base.OnPropertyChanged(change);
-
-            if (change.Property == IsToolWindowProperty)
-            {
-                UpdatePseudoClasses(change.NewValue.GetValueOrDefault<bool>());
-            }
-        }
-
-        private void UpdatePseudoClasses(bool isToolWindow)
-        {
-            PseudoClasses.Set(":toolwindow", isToolWindow);
-        }
-
-        /// <inheritdoc/>
-        protected override void OnPointerPressed(PointerPressedEventArgs e)
-        {
-            base.OnPointerPressed(e);
-
-            if (_chromeGrip is { } && _chromeGrip.IsPointerOver)
-            {
-                _mouseDown = true;
-                _startPoint = e.GetPosition(this);
-
-                if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed && !s_useCustomDrag)
-                {
-                    BeginMoveDrag(e);
-                    _mouseDown = false;
-                }
-            }
-            else
-            {
-                _mouseDown = false;
-            }
-        }
-
-        /// <inheritdoc/>
-        protected override void OnPointerReleased(PointerReleasedEventArgs e)
-        {
-            base.OnPointerReleased(e);
-
-            _mouseDown = false;
-        }
-
-        /// <inheritdoc/>
-        protected override void OnPointerMoved(PointerEventArgs e)
-        {
-            base.OnPointerMoved(e);
-
-            if (_chromeGrip is { } && _chromeGrip.IsPointerOver && _mouseDown && s_useCustomDrag)
-            {
-                // Using custom method because BeginMoveDrag is releasing pointer capture on Windows.
-                var point = e.GetPosition(this);
-                var delta = point - _startPoint;
-                var x = Position.X + delta.X;
-                var y = Position.Y + delta.Y;
-                Position = Position.WithX((int)x).WithY((int)y);
-                _startPoint = new Point(point.X - delta.X, point.Y - delta.Y);
             }
         }
 

53a9641f8d215d5aad1063044f5b78b7ece44d1c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-11 23:43:56 +0200

    Update AssemblyInfo.cs

--- src/Dock.Avalonia/Properties/AssemblyInfo.cs
+++ src/Dock.Avalonia/Properties/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 ﻿using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Dock.Avalonia")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Dock.Avalonia.Controls")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Dock.Avalonia.Converters")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Dock.Avalonia.MarkupExtension")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Dock.Avalonia.Themes")]

8fce9c3cce9c77c1336e21ecf693c70c452a84c5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-11 23:41:48 +0200

    Create LoadExtension.cs

---
+++ src/Dock.Avalonia/MarkupExtension/LoadExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+
+namespace Dock.Avalonia.MarkupExtension
+{
+    /// <summary>
+    /// Loads XAML <see cref="object"/> from a URI.
+    /// </summary>
+    public class LoadExtension
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadExtension"/> class.
+        /// </summary>
+        public LoadExtension()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadExtension"/> class.
+        /// </summary>
+        /// <param name="source">The source uri.</param>
+        public LoadExtension(Uri source)
+        {
+            Source = source;
+        }
+
+        /// <summary>
+        /// Provides a loaded <see cref="object"/> instance.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <returns>The loaded <see cref="object"/> instance.</returns>
+        public object? ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (serviceProvider.GetService(typeof(IUriContext)) is IUriContext uriContext)
+            {
+                var baseUri = uriContext.BaseUri;
+                var obj = AvaloniaXamlLoader.Load(Source, baseUri);
+                return obj;
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// Gets or sets the source URL.
+        /// </summary>
+        [ConstructorArgument("source")]
+        public Uri? Source { get; set; }
+    }
+}

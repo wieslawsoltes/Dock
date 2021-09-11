92d35a2556d15ae3cea73d444291410ca9f1fda2
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-11 23:44:12 +0200

    Add AvaloniaDemo.Xaml

---
+++ samples/AvaloniaDemo.Xaml/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Avalonia;
+using Dock.Model.Avalonia;
+
+namespace AvaloniaDemo.Xaml
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            GC.KeepAlive(typeof(Factory).Assembly);
+
+            return AppBuilder.Configure<App>()
+                             .UsePlatformDetect()
+                             .LogToTrace();
+        }
+    }
+}
---
+++ samples/AvaloniaDemo.Xaml/Views/MainView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaDemo.Xaml.Views
+{
+    public class MainView : UserControl
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}

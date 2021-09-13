663d2fac4785f4bd1243b2a22a5c995ff45ba550
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-13 08:14:44 +0200

    Do not hide create button when available

--- src/Dock.Avalonia/Controls/DockTabStrip.cs
+++ src/Dock.Avalonia/Controls/DockTabStrip.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Generators;
+using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
 using Avalonia.Styling;
 
@@ -9,9 +11,33 @@ namespace Dock.Avalonia.Controls
     /// <summary>
     /// Dock TabStrip custom control.
     /// </summary>
+    [PseudoClasses(":create")]
     public class DockTabStrip : TabStrip, IStyleable
     {
+        /// <summary>
+        /// Defines the <see cref="CanCreateItem"/> property.
+        /// </summary>
+        public static readonly StyledProperty<bool> CanCreateItemProperty =
+            AvaloniaProperty.Register<DockPanel, bool>(nameof(CanCreateItem));
+
+        /// <summary>
+        /// Gets or sets if tab strop dock can create new items.
+        /// </summary>
+        public bool CanCreateItem
+        {
+            get => GetValue(CanCreateItemProperty);
+            set => SetValue(CanCreateItemProperty, value);
+        }
+
         Type IStyleable.StyleKey => typeof(TabStrip);
+
+        /// <summary>
+        /// Initializes new instance of the <see cref="DockTabStrip"/> class.
+        /// </summary>
+        public DockTabStrip()
+        {
+            UpdatePseudoClasses(CanCreateItem);
+        }
 
         /// <inheritdoc/>
         protected override IItemContainerGenerator CreateItemContainerGenerator()
@@ -20,6 +46,22 @@ namespace Dock.Avalonia.Controls
                 this,
                 ContentControl.ContentProperty,
                 ContentControl.ContentTemplateProperty);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == CanCreateItemProperty)
+            {
+                UpdatePseudoClasses(change.NewValue.GetValueOrDefault<bool>());
+            }
+        }
+
+        private void UpdatePseudoClasses(bool canCreate)
+        {
+            PseudoClasses.Set(":create", canCreate);
         }
     }
 }

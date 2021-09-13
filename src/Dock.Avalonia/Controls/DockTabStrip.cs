d8cdab737501ccec08b22a2bfcb445c2fc7164cb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-13 07:59:04 +0200

    Add custom tab strip control

---
+++ src/Dock.Avalonia/Controls/DockTabStrip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Generators;
+using Avalonia.Controls.Primitives;
+using Avalonia.Styling;
+
+namespace Dock.Avalonia.Controls
+{
+    /// <summary>
+    /// Dock TabStrip custom control.
+    /// </summary>
+    public class DockTabStrip : TabStrip, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(TabStrip);
+
+        /// <inheritdoc/>
+        protected override IItemContainerGenerator CreateItemContainerGenerator()
+        {
+            return new ItemContainerGenerator<DockTabStripItem>(
+                this,
+                ContentControl.ContentProperty,
+                ContentControl.ContentTemplateProperty);
+        }
+    }
+}
---
+++ src/Dock.Avalonia/Controls/DockTabStripItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Avalonia.Controls.Primitives;
+using Avalonia.Styling;
+
+namespace Dock.Avalonia.Controls
+{
+    /// <summary>
+    /// Dock TabStripItem custom control.
+    /// </summary>
+    public class DockTabStripItem : TabStripItem, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(TabStripItem);
+    }
+}

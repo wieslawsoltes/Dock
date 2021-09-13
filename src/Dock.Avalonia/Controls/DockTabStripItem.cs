631327ecf2523d3bd88515d6bb32676660a2a126
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-09-13 07:59:36 +0200

    Close tabs on middle pointer press

--- src/Dock.Avalonia/Controls/DockTabStripItem.cs
+++ src/Dock.Avalonia/Controls/DockTabStripItem.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Styling;
+using Dock.Model.Core;
 
 namespace Dock.Avalonia.Controls
 {
@@ -10,5 +14,24 @@ namespace Dock.Avalonia.Controls
     public class DockTabStripItem : TabStripItem, IStyleable
     {
         Type IStyleable.StyleKey => typeof(TabStripItem);
+        
+        /// <inheritdoc/>
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToVisualTree(e);
+
+            AddHandler(PointerPressedEvent, PressedHandler, RoutingStrategies.Tunnel);
+        }
+
+        private void PressedHandler(object? sender, PointerPressedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsMiddleButtonPressed)
+            {
+                if (DataContext is IDockable { Owner: IDock { Factory: { } factory } } dockable)
+                {
+                    factory.CloseDockable(dockable);
+                }
+            }
+        }
     }
 }

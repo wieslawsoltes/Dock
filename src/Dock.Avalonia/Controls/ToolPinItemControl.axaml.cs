ffb71bbf24e173329cda1363569ab47daafa17c1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-23 22:19:24 +0100

    Add ToolPinItemControl

---
+++ src/Dock.Avalonia/Controls/ToolPinItemControl.axaml.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Controls.Primitives;
+
+namespace Dock.Avalonia.Controls;
+
+/// <summary>
+/// Interaction logic for <see cref="ToolPinItemControl"/> xaml.
+/// </summary>
+public class ToolPinItemControl : TemplatedControl
+{
+}

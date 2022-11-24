27e11464c8ce9ee1ff4268357f4745b4c82d52ed
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-24 13:44:27 +0100

    Update ToolPinnedControl.axaml.cs

--- src/Dock.Avalonia/Controls/ToolPinnedControl.axaml.cs
+++ src/Dock.Avalonia/Controls/ToolPinnedControl.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls.Primitives;
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Layout;
 
 namespace Dock.Avalonia.Controls;
 
@@ -7,4 +9,18 @@ namespace Dock.Avalonia.Controls;
 /// </summary>
 public class ToolPinnedControl : TemplatedControl
 {
+    /// <summary>
+    /// Defines the <see cref="Orientation"/> property.
+    /// </summary>
+    public static readonly StyledProperty<Orientation> OrientationProperty =
+        AvaloniaProperty.Register<ToolPinnedControl, Orientation>(nameof(Orientation), Orientation.Vertical);
+
+    /// <summary>
+    /// Gets or sets the orientation in which child controls will be layed out.
+    /// </summary>
+    public Orientation Orientation
+    {
+        get { return GetValue(OrientationProperty); }
+        set { SetValue(OrientationProperty, value); }
+    }
 }

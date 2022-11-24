0edd82335db3e13bb4d9d1d2fa9e95aa3892a063
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-24 22:09:36 +0100

    Add Items property

--- src/Dock.Avalonia/Controls/ToolPinnedControl.axaml.cs
+++ src/Dock.Avalonia/Controls/ToolPinnedControl.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia;
+﻿using System.Collections;
+using Avalonia;
+using Avalonia.Collections;
 using Avalonia.Controls.Primitives;
 using Avalonia.Layout;
 
@@ -10,17 +12,37 @@ namespace Dock.Avalonia.Controls;
 public class ToolPinnedControl : TemplatedControl
 {
     /// <summary>
+    /// Defines the <see cref="Items"/> property.
+    /// </summary>
+    public static readonly DirectProperty<ToolPinnedControl, IEnumerable?> ItemsProperty =
+        AvaloniaProperty.RegisterDirect<ToolPinnedControl, IEnumerable?>(
+            nameof(Items), 
+            o => o.Items, 
+            (o, v) => o.Items = v);
+
+    /// <summary>
     /// Defines the <see cref="Orientation"/> property.
     /// </summary>
     public static readonly StyledProperty<Orientation> OrientationProperty =
         AvaloniaProperty.Register<ToolPinnedControl, Orientation>(nameof(Orientation), Orientation.Vertical);
+
+    private IEnumerable? _items = new AvaloniaList<object>();
+
+    /// <summary>
+    /// Gets or sets the items to display.
+    /// </summary>
+    public IEnumerable? Items
+    {
+        get { return _items; }
+        set { SetAndRaise(ItemsProperty, ref _items, value); }
+    }
 
     /// <summary>
     /// Gets or sets the orientation in which child controls will be layed out.
     /// </summary>
     public Orientation Orientation
     {
-        get { return GetValue(OrientationProperty); }
-        set { SetValue(OrientationProperty, value); }
+        get => GetValue(OrientationProperty);
+        set => SetValue(OrientationProperty, value);
     }
 }

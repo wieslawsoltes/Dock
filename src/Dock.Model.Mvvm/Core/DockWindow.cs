a1cacdb7b02adf872ae26ed9518fffa18e9c292b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-28 13:08:40 +0100

    Update DockWindow.cs

--- src/Dock.Model.Mvvm/Core/DockWindow.cs
+++ src/Dock.Model.Mvvm/Core/DockWindow.cs
@@ -19,7 +19,7 @@ public class DockWindow : ObservableObject, IDockWindow
     private double _width;
     private double _height;
     private bool _topmost;
-    private string _title = string.Empty;
+    private string _title;
     private IDockable? _owner;
     private IFactory? _factory;
     private IRootDock? _layout;
@@ -31,6 +31,7 @@ public class DockWindow : ObservableObject, IDockWindow
     public DockWindow()
     {
         _id = nameof(IDockWindow);
+        _title = nameof(IDockWindow);
         _hostAdapter = new HostAdapter(this);
     }
 

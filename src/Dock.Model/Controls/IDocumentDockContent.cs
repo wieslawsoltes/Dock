1710cd5f2a2856e9d83b6f433e5a509166f0fedd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-28 13:11:20 +0100

    Create IDocumentDockContent.cs

---
+++ src/Dock.Model/Controls/IDocumentDockContent.cs
@@ -0,0 +1,20 @@
+﻿using Dock.Model.Core;
+
+namespace Dock.Model.Controls;
+
+/// <summary>
+/// Document dock content contract.
+/// </summary>
+public interface IDocumentDockContent : IDock
+{
+    /// <summary>
+    /// Gets or sets document template.
+    /// </summary>
+    IDocumentTemplate? DocumentTemplate { get; set; }
+
+    /// <summary>
+    /// Create new document from template.
+    /// </summary>
+    /// <returns>The new document instance.</returns>
+    object? CreateDocumentFromTemplate();
+}

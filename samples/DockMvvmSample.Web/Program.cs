1dc36488d63c03d3515dca11f0a0588ca7601165
Ichthus1604 <98904713+ichthus1604@users.noreply.github.com>
2023-02-11 09:59:43 -0300

    Add WASM project

---
+++ samples/DockMvvmSample.Web/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Versioning;
+using Avalonia;
+using Avalonia.Browser;
+using DockMvvmSample;
+
+[assembly: SupportedOSPlatform("browser")]
+
+
+internal partial class Program
+{
+    private static void Main(string[] args) => BuildAvaloniaApp()
+        .SetupBrowserApp("out");
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>();
+}

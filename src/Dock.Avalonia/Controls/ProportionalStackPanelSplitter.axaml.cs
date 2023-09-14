bb027c02175971939bae22324e44c1813b1c931d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-09-14 21:27:00 +0200

    Handle IsEmpty property in Avalonia controls

--- src/Dock.Avalonia/Controls/ProportionalStackPanelSplitter.axaml.cs
+++ src/Dock.Avalonia/Controls/ProportionalStackPanelSplitter.axaml.cs
@@ -43,6 +43,32 @@ public class ProportionalStackPanelSplitter : Thumb
     }
 
     /// <summary>
+    /// Defines the IsEmpty attached property.
+    /// </summary>
+    public static readonly AttachedProperty<bool> IsEmptyProperty =
+        AvaloniaProperty.RegisterAttached<ProportionalStackPanelSplitter, Control, bool>("IsEmpty", false, false, BindingMode.TwoWay);
+
+    /// <summary>
+    /// Gets the value of the IsEmpty attached property on the specified control.
+    /// </summary>
+    /// <param name="control">The control.</param>
+    /// <returns>The IsEmpty attached property.</returns>
+    public static bool GetIsEmpty(AvaloniaObject control)
+    {
+        return control.GetValue(IsEmptyProperty);
+    }
+
+    /// <summary>
+    /// Sets the value of the IsEmpty attached property on the specified control.
+    /// </summary>
+    /// <param name="control">The control.</param>
+    /// <param name="value">The value of the IsEmpty property.</param>
+    public static void SetIsEmpty(AvaloniaObject control, bool value)
+    {
+        control.SetValue(IsEmptyProperty, value);
+    }
+
+    /// <summary>
     /// Defines the <see cref="Thickness"/> property.
     /// </summary>
     public static readonly StyledProperty<double> ThicknessProperty =
@@ -148,6 +174,31 @@ public class ProportionalStackPanelSplitter : Thumb
         }
 
         return double.NaN;
+    }
+
+    internal static bool GetControlIsEmpty(Control? control)
+    {
+        if (control is ContentPresenter contentPresenter)
+        {
+            if (contentPresenter.Child is null)
+            {
+                contentPresenter.UpdateChild();
+            }
+
+            if (contentPresenter.Child is not null)
+            {
+                return GetIsEmpty(contentPresenter.Child);
+            }
+
+            return false;
+        }
+
+        if (control is not null)
+        {
+            return GetIsEmpty(control);
+        }
+
+        return false;
     }
 
     /// <inheritdoc/>

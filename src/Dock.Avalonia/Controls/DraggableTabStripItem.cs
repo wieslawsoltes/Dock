65351ecbd5887317d9fa7c66d1ceb32219fb1d19
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-07-01 11:20:14 +0200

    Refine tab dragging behavior

--- src/Dock.Avalonia/Controls/DraggableTabStripItem.cs
+++ src/Dock.Avalonia/Controls/DraggableTabStripItem.cs
@@ -1,8 +1,11 @@
 using System;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media.Transformation;
 using Avalonia.VisualTree;
 using Dock.Model.Core;
 using Dock.Settings;
@@ -10,15 +13,21 @@ using Dock.Settings;
 namespace Dock.Avalonia.Controls;
 
 /// <summary>
-/// Base class for TabStripItems that support dragging and floating.
+/// Base class for TabStripItems that support dragging, reordering and floating.
 /// </summary>
 /// <typeparam name="TTabStrip">Type of parent TabStrip.</typeparam>
 public abstract class DraggableTabStripItem<TTabStrip> : TabStripItem
     where TTabStrip : TabStrip
 {
-    private bool _pressed;
+    private bool _enableDrag;
+    private bool _dragStarted;
     private bool _detached;
+    private bool _captured;
     private Point _start;
+    private int _draggedIndex;
+    private int _targetIndex;
+    private TTabStrip? _tabStrip;
+    private TabStripItem? _draggedContainer;
 
     /// <inheritdoc/>
     protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
@@ -28,6 +37,7 @@ public abstract class DraggableTabStripItem<TTabStrip> : TabStripItem
         AddHandler(PointerPressedEvent, PressedHandler, RoutingStrategies.Tunnel);
         AddHandler(PointerMovedEvent, MovedHandler, RoutingStrategies.Tunnel);
         AddHandler(PointerReleasedEvent, ReleasedHandler, RoutingStrategies.Tunnel);
+        AddHandler(PointerCaptureLostEvent, CaptureLostHandler, RoutingStrategies.Tunnel);
     }
 
     /// <inheritdoc/>
@@ -38,11 +48,13 @@ public abstract class DraggableTabStripItem<TTabStrip> : TabStripItem
         RemoveHandler(PointerPressedEvent, PressedHandler);
         RemoveHandler(PointerMovedEvent, MovedHandler);
         RemoveHandler(PointerReleasedEvent, ReleasedHandler);
+        RemoveHandler(PointerCaptureLostEvent, CaptureLostHandler);
     }
 
     private void PressedHandler(object? sender, PointerPressedEventArgs e)
     {
-        if (e.GetCurrentPoint(this).Properties.IsMiddleButtonPressed)
+        var properties = e.GetCurrentPoint(this).Properties;
+        if (properties.IsMiddleButtonPressed)
         {
             if (DataContext is IDockable { Owner: IDock { Factory: { } factory }, CanClose: true } dockable)
             {
@@ -50,47 +62,208 @@ public abstract class DraggableTabStripItem<TTabStrip> : TabStripItem
             }
         }
 
-        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        if (properties.IsLeftButtonPressed && this.FindAncestorOfType<TTabStrip>() is { } tabStrip)
         {
-            _pressed = true;
+            _enableDrag = true;
+            _dragStarted = false;
             _detached = false;
-            _start = e.GetPosition(this);
+            _start = e.GetPosition(tabStrip);
+            _draggedIndex = -1;
+            _targetIndex = -1;
+            _tabStrip = tabStrip;
+            _draggedContainer = this;
+            SetDraggingPseudoClasses(this, true);
+            AddTransforms(tabStrip);
+            _captured = true;
+            e.Pointer.Capture(this);
         }
+    }
+
+    private void ReleasedHandler(object? sender, PointerReleasedEventArgs e)
+    {
+        if (_captured && e.InitialPressMouseButton == MouseButton.Left)
+        {
+            Released();
+        }
+        _captured = false;
+    }
+
+    private void CaptureLostHandler(object? sender, PointerCaptureLostEventArgs e)
+    {
+        Released();
+        _captured = false;
+    }
+
+    private void Released()
+    {
+        if (!_enableDrag)
+        {
+            return;
+        }
+
+        RemoveTransforms(_tabStrip);
+
+        if (_tabStrip is not null)
+        {
+            foreach (var control in _tabStrip.GetRealizedContainers())
+            {
+                SetDraggingPseudoClasses(control, true);
+            }
+        }
+
+        if (_dragStarted && !_detached && _draggedIndex >= 0 && _targetIndex >= 0 && _draggedIndex != _targetIndex)
+        {
+            MoveDockable(_draggedIndex, _targetIndex);
+        }
+
+        if (_tabStrip is not null)
+        {
+            foreach (var control in _tabStrip.GetRealizedContainers())
+            {
+                SetDraggingPseudoClasses(control, false);
+            }
+        }
+
+        if (_draggedContainer is not null)
+        {
+            SetDraggingPseudoClasses(_draggedContainer, false);
+        }
+
+        _draggedIndex = -1;
+        _targetIndex = -1;
+        _enableDrag = false;
+        _dragStarted = false;
+        _tabStrip = null;
+        _draggedContainer = null;
     }
 
     private void MovedHandler(object? sender, PointerEventArgs e)
     {
-        if (_pressed && !_detached)
+        if (!_captured || !_enableDrag || _tabStrip?.Items is null || _draggedContainer?.RenderTransform is null)
         {
-            var position = e.GetPosition(this);
-            var diff = position - _start;
-            if (Math.Abs(diff.X) > DockSettings.MinimumHorizontalDragDistance ||
-                Math.Abs(diff.Y) > DockSettings.MinimumVerticalDragDistance)
-            {
-                if (this.FindAncestorOfType<TTabStrip>() is { } tabStrip)
-                {
-                    var pt = e.GetPosition(tabStrip);
+            return;
+        }
 
-                    if (tabStrip.Bounds.Contains(pt))
-                    {
-                        ReorderDockable(tabStrip, pt);
-                    }
-                    else if (Math.Abs(diff.X) > DockSettings.FloatDragDistance ||
-                             Math.Abs(diff.Y) > DockSettings.FloatDragDistance)
-                    {
-                        if (DataContext is IDockable { Owner: IDock { Factory: { } factory } } dockable)
-                        {
-                            factory.FloatDockable(dockable);
-                            _detached = true;
-                        }
-                    }
-                }
+        var properties = e.GetCurrentPoint(this).Properties;
+        if (!properties.IsLeftButtonPressed)
+        {
+            return;
+        }
+
+        var position = e.GetPosition(_tabStrip);
+        var delta = position.X - _start.X;
+
+        if (!_dragStarted)
+        {
+            var diff = _start - position;
+            if (Math.Abs(diff.X) > DockSettings.MinimumHorizontalDragDistance)
+            {
+                _dragStarted = true;
             }
+            else
+            {
+                return;
+            }
+        }
+
+        if (!_detached && !_tabStrip.Bounds.Contains(position) &&
+            (Math.Abs(delta) > DockSettings.FloatDragDistance || Math.Abs(_start.Y - position.Y) > DockSettings.FloatDragDistance))
+        {
+            if (DataContext is IDockable { Owner: IDock { Factory: { } factory } } dockable)
+            {
+                factory.FloatDockable(dockable);
+                _detached = true;
+            }
+            return;
+        }
+
+        SetTranslateTransform(_draggedContainer, delta, 0);
+
+        _draggedIndex = _tabStrip.IndexFromContainer(_draggedContainer);
+        _targetIndex = -1;
+
+        var draggedBounds = _draggedContainer.Bounds;
+        var draggedStart = draggedBounds.X;
+        var draggedDeltaStart = draggedBounds.X + delta;
+        var draggedDeltaEnd = draggedBounds.X + delta + draggedBounds.Width;
+
+        var i = 0;
+        foreach (var _ in _tabStrip.Items)
+        {
+            var targetContainer = _tabStrip.ItemContainerGenerator.ContainerFromIndex(i) as TabStripItem;
+            if (targetContainer?.RenderTransform is null || ReferenceEquals(targetContainer, _draggedContainer))
+            {
+                i++;
+                continue;
+            }
+
+            var targetBounds = targetContainer.Bounds;
+            var targetStart = targetBounds.X;
+            var targetMid = targetBounds.X + targetBounds.Width / 2;
+            var targetIndex = _tabStrip.IndexFromContainer(targetContainer);
+
+            if (targetStart > draggedStart && draggedDeltaEnd >= targetMid)
+            {
+                SetTranslateTransform(targetContainer, -draggedBounds.Width, 0);
+                _targetIndex = _targetIndex == -1 ? targetIndex : Math.Max(targetIndex, _targetIndex);
+            }
+            else if (targetStart < draggedStart && draggedDeltaStart <= targetMid)
+            {
+                SetTranslateTransform(targetContainer, draggedBounds.Width, 0);
+                _targetIndex = _targetIndex == -1 ? targetIndex : Math.Min(targetIndex, _targetIndex);
+            }
+            else
+            {
+                SetTranslateTransform(targetContainer, 0, 0);
+            }
+
+            i++;
         }
     }
 
-    private void ReorderDockable(TabStrip tabStrip, Point pointer)
+    private void AddTransforms(ItemsControl? itemsControl)
     {
+        if (itemsControl?.Items is null)
+        {
+            return;
+        }
+
+        var i = 0;
+        foreach (var _ in itemsControl.Items)
+        {
+            if (itemsControl.ContainerFromIndex(i) is Control container)
+            {
+                SetTranslateTransform(container, 0, 0);
+            }
+            i++;
+        }
+    }
+
+    private void RemoveTransforms(ItemsControl? itemsControl)
+    {
+        if (itemsControl?.Items is null)
+        {
+            return;
+        }
+
+        var i = 0;
+        foreach (var _ in itemsControl.Items)
+        {
+            if (itemsControl.ContainerFromIndex(i) is Control container)
+            {
+                SetTranslateTransform(container, 0, 0);
+            }
+            i++;
+        }
+    }
+
+    private void MoveDockable(int draggedIndex, int targetIndex)
+    {
+        if (_tabStrip is null)
+        {
+            return;
+        }
+
         if (DataContext is not IDockable { Owner: IDock { Factory: { } factory } owner } dockable)
         {
             return;
@@ -101,38 +274,39 @@ public abstract class DraggableTabStripItem<TTabStrip> : TabStripItem
             return;
         }
 
-        var from = list.IndexOf(dockable);
-        for (var i = 0; i < list.Count; i++)
+        if (targetIndex >= list.Count)
         {
-            if (tabStrip.ItemContainerGenerator.ContainerFromIndex(i) is not TabStripItem container)
-            {
-                continue;
-            }
-
-            var bounds = container.Bounds;
-            var mid = bounds.X + bounds.Width / 2;
-            if (pointer.X < mid)
-            {
-                if (i != from && list[i] is IDockable target)
-                {
-                    factory.MoveDockable(owner, owner, dockable, target);
-                }
-                return;
-            }
+            targetIndex = list.Count - 1;
         }
 
-        if (list.Count > 0 && from != list.Count - 1)
+        if (targetIndex < 0 || targetIndex >= list.Count)
         {
-            if (list[list.Count - 1] is IDockable last)
-            {
-                factory.MoveDockable(owner, owner, dockable, last);
-            }
+            return;
+        }
+
+        var target = list[targetIndex];
+        if (!ReferenceEquals(target, dockable))
+        {
+            factory.MoveDockable(owner, owner, dockable, target);
         }
     }
 
-    private void ReleasedHandler(object? sender, PointerReleasedEventArgs e)
+    private void SetDraggingPseudoClasses(Control control, bool isDragging)
     {
-        _pressed = false;
-        _detached = false;
+        if (isDragging)
+        {
+            ((IPseudoClasses)control.Classes).Add(":dragging");
+        }
+        else
+        {
+            ((IPseudoClasses)control.Classes).Remove(":dragging");
+        }
+    }
+
+    private void SetTranslateTransform(Control control, double x, double y)
+    {
+        var builder = new TransformOperations.Builder(1);
+        builder.AppendTranslate(x, y);
+        control.RenderTransform = builder.Build();
     }
 }

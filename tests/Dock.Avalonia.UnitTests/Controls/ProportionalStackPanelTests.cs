add6a3fee397717e44b8cb9db8e1dc130978b681
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-01 23:57:59 +0200

    Update ProportionalStackPanelTests.cs

--- tests/Dock.Avalonia.UnitTests/Controls/ProportionalStackPanelTests.cs
+++ tests/Dock.Avalonia.UnitTests/Controls/ProportionalStackPanelTests.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Layout;
 using Avalonia.Markup.Xaml.Templates;
 using Avalonia.Media;
-using Dock.Avalonia.Controls;
+using Dock.Controls.ProportionalStackPanel;
 using Xunit;
 
 namespace Dock.Avalonia.UnitTests.Controls;

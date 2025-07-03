0c8a59727d8760e34a5f745e4fd5cab3fd3bf1d0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-03 21:15:44 +0200

    Fix

---
+++ src/Dock.Avalonia/Converters/DocumentTabDockConverter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using AC = Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Dock.Model.Core;
+
+namespace Dock.Avalonia.Converters;
+
+/// <summary>
+/// Converts <see cref="DocumentTabLayout"/> to Avalonia <see cref="Dock"/>.
+/// </summary>
+public class DocumentTabDockConverter : IValueConverter
+{
+    /// <summary>
+    /// Gets <see cref="DocumentTabDockConverter"/> instance.
+    /// </summary>
+    public static readonly DocumentTabDockConverter Instance = new ();
+
+    /// <inheritdoc/>
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            DocumentTabLayout.Left => AC.Dock.Left,
+            DocumentTabLayout.Right => AC.Dock.Right,
+            DocumentTabLayout.Top => AC.Dock.Top,
+            _ => AvaloniaProperty.UnsetValue
+        };
+    }
+
+    /// <inheritdoc/>
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return AvaloniaProperty.UnsetValue;
+    }
+}

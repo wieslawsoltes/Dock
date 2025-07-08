63dcda189e083fe2bf23931450e4136c8c24820e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-08 12:45:51 +0200

    Add WebViewSample

---
+++ samples/WebViewSample/App.axaml.cs
@@ -0,0 +1,31 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace WebViewSample;
+
+public class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+        {
+            desktopLifetime.MainWindow = new MainWindow();
+        }
+
+        if (ApplicationLifetime is ISingleViewApplicationLifetime singleLifetime)
+        {
+            singleLifetime.MainView = new MainView();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+}

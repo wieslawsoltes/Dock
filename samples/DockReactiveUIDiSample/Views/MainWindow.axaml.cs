ab71e9bd278bf0aeb747b14f6768146ae2b91ee5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-11 20:29:04 +0200

    Update MainWindow.axaml.cs

--- samples/DockReactiveUIDiSample/Views/MainWindow.axaml.cs
+++ samples/DockReactiveUIDiSample/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 
 namespace DockReactiveUIDiSample.Views;
 
@@ -7,5 +8,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
     }
 }

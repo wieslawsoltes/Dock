a9aa615d1c406fe57e53080ff709196312413982
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-07-11 13:54:12 +0200

    Add test verifying adorner reset when leaving dock

--- tests/Dock.Avalonia.HeadlessTests/HostWindowStateTests.cs
+++ tests/Dock.Avalonia.HeadlessTests/HostWindowStateTests.cs
@@ -3,6 +3,10 @@ using Avalonia.Headless.XUnit;
 using Dock.Avalonia.Controls;
 using Dock.Avalonia.Internal;
 using Dock.Model;
+using Dock.Model.Avalonia;
+using Dock.Model.Avalonia.Core;
+using System.Reflection;
+using Avalonia.Controls;
 using Xunit;
 
 namespace Dock.Avalonia.HeadlessTests;
@@ -40,5 +44,38 @@ public class HostWindowStateTests
         state.Process(new PixelPoint(), EventType.Released);
 
         Assert.NotNull(window.HostWindowState);
+    }
+
+    [AvaloniaFact]
+    public void Move_OutsideDockControl_Resets_DropControl()
+    {
+        var manager = new DockManager();
+        var window = new HostWindow();
+        var factory = new Factory();
+        var layout = factory.CreateRootDock();
+        var dockWindow = new DockWindow { Factory = factory, Layout = layout };
+        window.Window = dockWindow;
+        var state = CreateState(manager, window);
+
+        state.Process(new PixelPoint(0,0), EventType.Pressed);
+        state.Process(new PixelPoint(10,10), EventType.Moved);
+
+        var dropControlProperty = typeof(DockManagerState)
+            .GetProperty("DropControl", BindingFlags.Instance | BindingFlags.NonPublic);
+        dropControlProperty!.SetValue(state, new Control());
+
+        var contextField = typeof(HostWindowState)
+            .GetField("_context", BindingFlags.Instance | BindingFlags.NonPublic)!;
+        var context = contextField.GetValue(state)!;
+        context.GetType().GetProperty("TargetDockControl")!.SetValue(context, new DockControl());
+        context.GetType().GetProperty("TargetPoint")!.SetValue(context, new Point(5,5));
+        context.GetType().GetProperty("DoDragDrop")!.SetValue(context, true);
+        context.GetType().GetProperty("PointerPressed")!.SetValue(context, true);
+
+        state.Process(new PixelPoint(20,20), EventType.Moved);
+
+        Assert.Null(dropControlProperty.GetValue(state));
+        Assert.Null(context.GetType().GetProperty("TargetDockControl")!.GetValue(context));
+        Assert.Equal(default(Point), (Point)context.GetType().GetProperty("TargetPoint")!.GetValue(context)!);
     }
 }

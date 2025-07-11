06a5dd16da5f12ab83d65b3d309999e5f79efae9
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-11 22:40:26 +0200

    Add headers

--- src/Dock.Avalonia/Contract/DockOperationHandler.cs
+++ src/Dock.Avalonia/Contract/DockOperationHandler.cs
@@ -1,3 +1,5 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 using Avalonia;
 using Avalonia.VisualTree;
 using Dock.Model.Core;

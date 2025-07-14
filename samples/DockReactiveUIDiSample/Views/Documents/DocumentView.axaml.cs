3f3d925ff0021ef03178d21a6e95c68b9ca84d1c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-07-14 12:25:29 +0200

    Use reactive base classes in DI sample

--- samples/DockReactiveUIDiSample/Views/Documents/DocumentView.axaml.cs
+++ samples/DockReactiveUIDiSample/Views/Documents/DocumentView.axaml.cs
@@ -1,33 +1,18 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using DockReactiveUIDiSample.ViewModels.Documents;
+using Avalonia.ReactiveUI;
 using ReactiveUI;
 
 namespace DockReactiveUIDiSample.Views.Documents;
 
-public partial class DocumentView : UserControl, IViewFor<DocumentViewModel>
+public partial class DocumentView : ReactiveUserControl<DocumentViewModel>
 {
     public DocumentView()
     {
         InitializeComponent();
     }
 
-    private DocumentViewModel? _viewModel;
-    public DocumentViewModel? ViewModel
-    {
-        get => _viewModel;
-        set
-        {
-            _viewModel = value;
-            DataContext = value;
-        }
-    }
-
-    object? IViewFor.ViewModel
-    {
-        get => ViewModel;
-        set => ViewModel = (DocumentViewModel?)value;
-    }
 
     private void InitializeComponent()
     {
--- samples/DockReactiveUIDiSample/Views/Tools/ToolView.axaml.cs
+++ samples/DockReactiveUIDiSample/Views/Tools/ToolView.axaml.cs
@@ -1,33 +1,18 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using DockReactiveUIDiSample.ViewModels.Tools;
+using Avalonia.ReactiveUI;
 using ReactiveUI;
 
 namespace DockReactiveUIDiSample.Views.Tools;
 
-public partial class ToolView : UserControl, IViewFor<ToolViewModel>
+public partial class ToolView : ReactiveUserControl<ToolViewModel>
 {
     public ToolView()
     {
         InitializeComponent();
     }
 
-    private ToolViewModel? _viewModel;
-    public ToolViewModel? ViewModel
-    {
-        get => _viewModel;
-        set
-        {
-            _viewModel = value;
-            DataContext = value;
-        }
-    }
-
-    object? IViewFor.ViewModel
-    {
-        get => ViewModel;
-        set => ViewModel = (ToolViewModel?)value;
-    }
 
     private void InitializeComponent()
     {

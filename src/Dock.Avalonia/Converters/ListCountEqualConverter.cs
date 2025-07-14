b415e932de73d928ca60cf7b927e70f8a301a3ce
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-14 23:25:22 +0200

    Add

---
+++ src/Dock.Avalonia/Converters/ListCountEqualConverter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Dock.Avalonia.Converters;
+
+/// <summary>
+/// Converts an <see cref="IList.Count"/> to a boolean indicating whether it is equal to a specified value.
+/// </summary>
+public class ListCountEqualConverter : IValueConverter
+{
+    /// <summary>
+    /// Gets or sets the value to compare against. The converter will return true if the input <see cref="IList.Count"/> is equal to this value.
+    /// </summary>
+    public int TrueIfEqual { get; set; }
+
+    /// <inheritdoc/>
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is IList list)
+        {
+            return list.Count == TrueIfEqual;
+        }
+        return false;
+    }
+
+    /// <inheritdoc/>
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}

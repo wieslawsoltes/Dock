20547e92ac8a4768a2c11afa656b9f14ec2ab8a5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-20 23:37:18 +0200

    Update App.axaml.cs

--- samples/DockInpcSample/App.axaml.cs
+++ samples/DockInpcSample/App.axaml.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Dock.Avalonia.Controls;
 using DockInpcSample.Themes;
 using DockInpcSample.ViewModels;
 using DockInpcSample.Views;
@@ -32,7 +34,11 @@ public class App : Application
                     {
                         DataContext = mainWindowViewModel
                     };
-
+#if DEBUG
+                    mainWindow.AttachDockDebug(
+                        mainWindowViewModel.Layout, 
+                        new KeyGesture(Key.F11));
+#endif
                     mainWindow.Closing += (_, _) =>
                     {
                         mainWindowViewModel.CloseLayout();

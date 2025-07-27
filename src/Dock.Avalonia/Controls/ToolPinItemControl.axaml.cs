c1db5da9d8741b0cd237950068b67cef5aeedb8d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-07-27 19:48:43 +0200

    Add drag support for pinned dockables

--- src/Dock.Avalonia/Controls/ToolPinItemControl.axaml.cs
+++ src/Dock.Avalonia/Controls/ToolPinItemControl.axaml.cs
@@ -1,8 +1,15 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
+using System;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
-using Avalonia.Layout;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+using Dock.Avalonia.Internal;
+using Dock.Model.Core;
+using AvaloniaOrientation = Avalonia.Layout.Orientation;
 
 namespace Dock.Avalonia.Controls;
 
@@ -11,18 +18,78 @@ namespace Dock.Avalonia.Controls;
 /// </summary>
 public class ToolPinItemControl : TemplatedControl
 {
+    private ItemDragHelper? _dragHelper;
+
     /// <summary>
     /// Defines the <see cref="Orientation"/> property.
     /// </summary>
-    public static readonly StyledProperty<Orientation> OrientationProperty =
-        AvaloniaProperty.Register<ToolPinItemControl, Orientation>(nameof(Orientation), Orientation.Vertical);
+    public static readonly StyledProperty<AvaloniaOrientation> OrientationProperty =
+        AvaloniaProperty.Register<ToolPinItemControl, AvaloniaOrientation>(nameof(Orientation), AvaloniaOrientation.Vertical);
 
     /// <summary>
     /// Gets or sets the orientation in which control will be layed out.
     /// </summary>
-    public Orientation Orientation
+    public AvaloniaOrientation Orientation
     {
         get => GetValue(OrientationProperty);
         set => SetValue(OrientationProperty, value);
+    }
+
+
+
+    private static DragAction ToDragAction(PointerEventArgs e)
+    {
+        if (e.KeyModifiers.HasFlag(KeyModifiers.Alt))
+        {
+            return DragAction.Link;
+        }
+
+        if (e.KeyModifiers.HasFlag(KeyModifiers.Shift))
+        {
+            return DragAction.Move;
+        }
+
+        if (e.KeyModifiers.HasFlag(KeyModifiers.Control))
+        {
+            return DragAction.Copy;
+        }
+
+        return DragAction.Move;
+    }
+
+    private void StartDockDrag(PointerEventArgs e)
+    {
+        var dockControl = this.FindAncestorOfType<DockControl>();
+        if (dockControl?.Layout?.Factory?.DockControls is { } dockControls
+            && dockControl.DockControlState is DockControlState state)
+        {
+            var position = e.GetPosition(dockControl);
+            var action = ToDragAction(e);
+            state.StartDrag(this, position, dockControl);
+            state.Process(position, default, EventType.Moved, action, dockControl, dockControls);
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+
+        _dragHelper = new ItemDragHelper(
+            this,
+            () => Parent as ItemsControl,
+            () => Orientation,
+            dragOutside: StartDockDrag,
+            getBoundsContainer: () => this.FindAncestorOfType<ToolPinnedControl>());
+        _dragHelper.Attach();
+    }
+
+    /// <inheritdoc/>
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+
+        _dragHelper?.Detach();
+        _dragHelper = null;
     }
 }
--- src/Dock.Avalonia/Controls/ToolPinnedControl.axaml.cs
+++ src/Dock.Avalonia/Controls/ToolPinnedControl.axaml.cs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
-using System.Collections;
+using System;
 using Avalonia;
-using Avalonia.Collections;
-using Avalonia.Controls.Primitives;
+using Avalonia.Controls;
 using Avalonia.Layout;
 
 namespace Dock.Avalonia.Controls;
@@ -11,33 +10,13 @@ namespace Dock.Avalonia.Controls;
 /// <summary>
 /// Interaction logic for <see cref="ToolPinnedControl"/> xaml.
 /// </summary>
-public class ToolPinnedControl : TemplatedControl
+public class ToolPinnedControl : ItemsControl
 {
-    /// <summary>
-    /// Defines the <see cref="Items"/> property.
-    /// </summary>
-    public static readonly DirectProperty<ToolPinnedControl, IEnumerable?> ItemsProperty =
-        AvaloniaProperty.RegisterDirect<ToolPinnedControl, IEnumerable?>(
-            nameof(Items), 
-            o => o.Items, 
-            (o, v) => o.Items = v);
-
     /// <summary>
     /// Defines the <see cref="Orientation"/> property.
     /// </summary>
     public static readonly StyledProperty<Orientation> OrientationProperty =
         AvaloniaProperty.Register<ToolPinnedControl, Orientation>(nameof(Orientation), Orientation.Vertical);
-
-    private IEnumerable? _items = new AvaloniaList<object>();
-
-    /// <summary>
-    /// Gets or sets the items to display.
-    /// </summary>
-    public IEnumerable? Items
-    {
-        get { return _items; }
-        set { SetAndRaise(ItemsProperty, ref _items, value); }
-    }
 
     /// <summary>
     /// Gets or sets the orientation in which child controls will be layed out.
@@ -46,5 +25,34 @@ public class ToolPinnedControl : TemplatedControl
     {
         get => GetValue(OrientationProperty);
         set => SetValue(OrientationProperty, value);
+    }
+
+    /// <inheritdoc/>
+    protected override Type StyleKeyOverride => typeof(ToolPinnedControl);
+
+    /// <inheritdoc/>
+    protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey)
+    {
+        return new ToolPinItemControl
+        {
+            Orientation = Orientation
+        };
+    }
+
+    /// <inheritdoc/>
+    protected override void PrepareContainerForItemOverride(Control container, object? item, int index)
+    {
+        base.PrepareContainerForItemOverride(container, item, index);
+
+        if (container is ToolPinItemControl pinItemControl)
+        {
+            pinItemControl.Orientation = Orientation;
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey)
+    {
+        return NeedsContainer<ToolPinItemControl>(item, out recycleKey);
     }
 }

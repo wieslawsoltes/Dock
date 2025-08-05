bf15c8b960f5e19b89c85d946812f7988f681f59
Dan Walmsley <dan@walms.co.uk>
2025-08-05 15:15:16 +0200

    fix the ProportionalDockControl template.

--- src/Dock.Avalonia/Controls/ProportionalDockControl.axaml.cs
+++ src/Dock.Avalonia/Controls/ProportionalDockControl.axaml.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
 using Avalonia.Data;
 using Dock.Controls.ProportionalStackPanel;
@@ -12,6 +13,7 @@ namespace Dock.Avalonia.Controls;
 /// <summary>
 /// Interaction logic for <see cref="ProportionalDockControl"/> xaml.
 /// </summary>
+[TemplatePart("PART_ItemsControl", typeof(ItemsControl), IsRequired = true)]
 public class ProportionalDockControl : TemplatedControl
 {
     private ItemsControl? _itemsControl;

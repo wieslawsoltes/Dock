a8e99a3fed48c964b07722e9c7b79d569b88ab85
Dan Walmsley <dan@walms.co.uk>
2025-08-05 13:57:11 +0200

    Bind the ProportionalStackPanel.Proportion attachedProperties in ContainerPrepared, to workaround bug in Avalonia causing bindings assigned from styles to not update.

--- src/Dock.Avalonia/Controls/ProportionalDockControl.axaml.cs
+++ src/Dock.Avalonia/Controls/ProportionalDockControl.axaml.cs
@@ -1,6 +1,11 @@
 // Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
+
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Dock.Controls.ProportionalStackPanel;
+using Dock.Model.Core;
 
 namespace Dock.Avalonia.Controls;
 
@@ -9,4 +14,33 @@ namespace Dock.Avalonia.Controls;
 /// </summary>
 public class ProportionalDockControl : TemplatedControl
 {
+    private ItemsControl? _itemsControl;
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        // Unsubscribe from the previous ItemsControl if it exists
+        if (_itemsControl != null)
+        {
+            _itemsControl.ContainerPrepared -= ItemsControlOnContainerPrepared;
+            _itemsControl = null;
+        }
+
+        base.OnApplyTemplate(e);
+
+        var itemsControl = e.NameScope.Find<ItemsControl>("PART_ItemsControl");
+
+        if (itemsControl != null)
+        {
+            _itemsControl = itemsControl;
+            _itemsControl.ContainerPrepared += ItemsControlOnContainerPrepared;
+        }
+    }
+
+    private void ItemsControlOnContainerPrepared(object sender, ContainerPreparedEventArgs e)
+    {
+        if (e.Container.DataContext is IDockable)
+        {
+            e.Container[!ProportionalStackPanel.ProportionProperty] = new Binding(nameof(IDockable.Proportion));
+        }
+    }
 }

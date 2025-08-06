e60e10db42d90ad26ee472265b5900622a25956b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-08-06 21:38:42 +0200

    Update DockDataTemplateHelper.cs

--- src/Dock.Avalonia/Helpers/DockDataTemplateHelper.cs
+++ src/Dock.Avalonia/Helpers/DockDataTemplateHelper.cs
@@ -24,8 +24,15 @@ internal static class DockDataTemplateHelper
     {
         yield return CreateDataTemplate<IDocumentContent>(() => new DocumentContentControl());
         yield return CreateDataTemplate<IToolContent>(() => new ToolContentControl());
-        yield return CreateProportionalSplitterDataTemplate();
-        yield return CreateGridSplitterDataTemplate();
+        yield return CreateDataTemplate<IProportionalDockSplitter>(() => new ProportionalStackPanelSplitter
+        {
+            [!ProportionalStackPanelSplitter.IsResizingEnabledProperty] = new Binding(nameof(IProportionalDockSplitter.CanResize)),
+            [!ProportionalStackPanelSplitter.PreviewResizeProperty] = new Binding(nameof(IProportionalDockSplitter.ResizePreview))
+        });
+        yield return CreateDataTemplate<IGridDockSplitter>(() => new GridSplitter
+        {
+            [!GridSplitter.ResizeDirectionProperty] = new Binding(nameof(IGridDockSplitter.ResizeDirection))
+        });
         yield return CreateDataTemplate<IDocumentDock>(() => new DocumentDockControl());
         yield return CreateDataTemplate<IToolDock>(() => new ToolDockControl());
         yield return CreateDataTemplate<IProportionalDock>(() => new ProportionalDockControl());
@@ -46,34 +53,5 @@ internal static class DockDataTemplateHelper
     private static FuncDataTemplate<T> CreateDataTemplate<T>(Func<Control> factory) where T : class
     {
         return new FuncDataTemplate<T>((_, _) => factory());
-    }
-
-    /// <summary>
-    /// Creates a DataTemplate for IProportionalDockSplitter with proper bindings.
-    /// </summary>
-    /// <returns>A DataTemplate for IProportionalDockSplitter.</returns>
-    private static FuncDataTemplate<IProportionalDockSplitter> CreateProportionalSplitterDataTemplate()
-    {
-        return new FuncDataTemplate<IProportionalDockSplitter>((_, _) =>
-        {
-            var splitter = new ProportionalStackPanelSplitter();
-            splitter.Bind(ProportionalStackPanelSplitter.IsResizingEnabledProperty, new Binding(nameof(IProportionalDockSplitter.CanResize)));
-            splitter.Bind(ProportionalStackPanelSplitter.PreviewResizeProperty, new Binding(nameof(IProportionalDockSplitter.ResizePreview)));
-            return splitter;
-        });
-    }
-
-    /// <summary>
-    /// Creates a DataTemplate for IGridDockSplitter with proper bindings.
-    /// </summary>
-    /// <returns>A DataTemplate for IGridDockSplitter.</returns>
-    private static FuncDataTemplate<IGridDockSplitter> CreateGridSplitterDataTemplate()
-    {
-        return new FuncDataTemplate<IGridDockSplitter>((_, _) =>
-        {
-            var splitter = new GridSplitter();
-            splitter.Bind(GridSplitter.ResizeDirectionProperty, new Binding(nameof(IGridDockSplitter.ResizeDirection)));
-            return splitter;
-        });
     }
 }

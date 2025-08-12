ca48d728f539f5a7742cbafe2b83f40eba7c18af
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-08-12 11:37:52 +0200

    Add DockSimplifiedFluentSample

---
+++ samples/DockSimplifiedFluentSample/Program.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using System;
+
+namespace DockSimplifiedFluentSample;
+
+class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
+}
+
+

d9e4a7b6eb5b664be9fb1cb1b90c62ae5f943ad4
Damian Suess <djsuess@gmail.com>
2025-09-04 10:23:43 -0400

    Code cleanup

--- samples/DockPrismSample/App.axaml.cs
+++ samples/DockPrismSample/App.axaml.cs
@@ -1,6 +1,11 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Dock.Avalonia.Diagnostics;
+using Dock.Avalonia.Diagnostics.Controls;
 using Dock.Model.Core;
 using Dock.Serializer;
 using DockPrismSample.Themes;
@@ -26,70 +31,48 @@ public partial class App : PrismApplication
         base.Initialize();
     }
 
+    /// <summary>Inform system which Window to use as the main app.</summary>
+    /// <returns>Avalonia Window.</returns>
     protected override AvaloniaObject CreateShell()
     {
         return Container.Resolve<MainWindow>();
     }
 
     /// <summary>Register you Services, Views, Dialogs, etc.</summary>
-    /// <param name="containerRegistry">container</param>
+    /// <param name="containerRegistry">container.</param>
     protected override void RegisterTypes(IContainerRegistry containerRegistry)
     {
         // Services
-        //// containerRegistry.RegisterSingleton<ISerialPortService, SerialPortService>();
         containerRegistry.RegisterSingleton<IFactory, DockFactory>();
         containerRegistry.RegisterSingleton<IDockSerializer, DockSerializer>();
 
-        // Views - Generic
+        // Windows
         containerRegistry.Register<MainWindow>();
 
         // Views
         //// containerRegistry.RegisterForNavigation<DashboardView, DashboardViewModel>();
     }
 
-    /*
     public override void OnFrameworkInitializationCompleted()
     {
         // DockManager.s_enableSplitToWindow = true;
-
-        var mainWindowViewModel = new MainWindowViewModel();
+        base.OnFrameworkInitializationCompleted();
 
         switch (ApplicationLifetime)
         {
             case IClassicDesktopStyleApplicationLifetime desktopLifetime:
             {
-                var mainWindow = new MainWindow
+                var win = (MainWindow as Window);
+                var vm = win?.DataContext as MainWindowViewModel;
+                if (win is not null && vm is not null)
                 {
-                    DataContext = mainWindowViewModel
-                };
 #if DEBUG
-                mainWindow.AttachDockDebug(
-                    () => mainWindowViewModel.Layout!,
-                    new KeyGesture(Key.F11));
-                mainWindow.AttachDockDebugOverlay(new KeyGesture(Key.F9));
+                    win.AttachDockDebug(() => vm.Layout!, new KeyGesture(Key.F11));
+                    win.AttachDockDebugOverlay(new KeyGesture(Key.F9));
 #endif
-                mainWindow.Closing += (_, _) =>
-                {
-                    mainWindowViewModel.CloseLayout();
-                };
-
-                desktopLifetime.MainWindow = mainWindow;
-
-                desktopLifetime.Exit += (_, _) =>
-                {
-                    mainWindowViewModel.CloseLayout();
-                };
-
-                break;
-            }
-            case ISingleViewApplicationLifetime singleViewLifetime:
-            {
-                var mainView = new MainView()
-                {
-                    DataContext = mainWindowViewModel
-                };
-
-                singleViewLifetime.MainView = mainView;
+                    win.Closing += (_, _) => { vm.CloseLayout(); };
+                    desktopLifetime.Exit += (_, _) => { vm.CloseLayout(); };
+                }
 
                 break;
             }
@@ -99,5 +82,4 @@ public partial class App : PrismApplication
         this.AttachDevTools();
 #endif
     }
-    */
 }
--- samples/DockPrismSample/ViewModels/MainWindowViewModel.cs
+++ samples/DockPrismSample/ViewModels/MainWindowViewModel.cs
@@ -40,70 +40,44 @@ public class MainWindowViewModel : BindableBase
 
     private void DebugFactoryEvents(IFactory factory)
     {
-        factory.ActiveDockableChanged += (_, args) =>
-        {
-            Debug.WriteLine($"[ActiveDockableChanged] Title='{args.Dockable?.Title}'");
-        };
+        factory.ActiveDockableChanged += (_, args)
+            => Debug.WriteLine($"[ActiveDockableChanged] Title='{args.Dockable?.Title}'");
 
-        factory.FocusedDockableChanged += (_, args) =>
-        {
-            Debug.WriteLine($"[FocusedDockableChanged] Title='{args.Dockable?.Title}'");
-        };
+        factory.FocusedDockableChanged += (_, args)
+            => Debug.WriteLine($"[FocusedDockableChanged] Title='{args.Dockable?.Title}'");
 
-        factory.DockableAdded += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableAdded] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockableAdded += (_, args)
+            => Debug.WriteLine($"[DockableAdded] Title='{args.Dockable?.Title}'");
 
-        factory.DockableRemoved += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableRemoved] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockableRemoved += (_, args)
+            => Debug.WriteLine($"[DockableRemoved] Title='{args.Dockable?.Title}'");
 
-        factory.DockableClosed += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableClosed] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockableClosed += (_, args)
+            => Debug.WriteLine($"[DockableClosed] Title='{args.Dockable?.Title}'");
 
-        factory.DockableMoved += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableMoved] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockableMoved += (_, args)
+            => Debug.WriteLine($"[DockableMoved] Title='{args.Dockable?.Title}'");
 
-        factory.DockableDocked += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableDocked] Title='{args.Dockable?.Title}', Operation='{args.Operation}'");
-        };
+        factory.DockableDocked += (_, args)
+            => Debug.WriteLine($"[DockableDocked] Title='{args.Dockable?.Title}', Operation='{args.Operation}'");
 
-        factory.DockableUndocked += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableUndocked] Title='{args.Dockable?.Title}', Operation='{args.Operation}'");
-        };
+        factory.DockableUndocked += (_, args)
+            => Debug.WriteLine($"[DockableUndocked] Title='{args.Dockable?.Title}', Operation='{args.Operation}'");
 
-        factory.DockableSwapped += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableSwapped] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockableSwapped += (_, args)
+            => Debug.WriteLine($"[DockableSwapped] Title='{args.Dockable?.Title}'");
 
-        factory.DockablePinned += (_, args) =>
-        {
-            Debug.WriteLine($"[DockablePinned] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockablePinned += (_, args)
+            => Debug.WriteLine($"[DockablePinned] Title='{args.Dockable?.Title}'");
 
-        factory.DockableUnpinned += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableUnpinned] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockableUnpinned += (_, args)
+            => Debug.WriteLine($"[DockableUnpinned] Title='{args.Dockable?.Title}'");
 
-        factory.WindowOpened += (_, args) =>
-        {
-            Debug.WriteLine($"[WindowOpened] Title='{args.Window?.Title}'");
-        };
+        factory.WindowOpened += (_, args)
+            => Debug.WriteLine($"[WindowOpened] Title='{args.Window?.Title}'");
 
-        factory.WindowClosed += (_, args) =>
-        {
-            Debug.WriteLine($"[WindowClosed] Title='{args.Window?.Title}'");
-        };
+        factory.WindowClosed += (_, args)
+            => Debug.WriteLine($"[WindowClosed] Title='{args.Window?.Title}'");
 
         factory.WindowClosing += (_, args) =>
         {
@@ -114,15 +88,11 @@ public class MainWindowViewModel : BindableBase
             Debug.WriteLine($"[WindowClosing] Title='{args.Window?.Title}', Cancel={args.Cancel}");
         };
 
-        factory.WindowAdded += (_, args) =>
-        {
-            Debug.WriteLine($"[WindowAdded] Title='{args.Window?.Title}'");
-        };
+        factory.WindowAdded += (_, args)
+            => Debug.WriteLine($"[WindowAdded] Title='{args.Window?.Title}'");
 
-        factory.WindowRemoved += (_, args) =>
-        {
-            Debug.WriteLine($"[WindowRemoved] Title='{args.Window?.Title}'");
-        };
+        factory.WindowRemoved += (_, args)
+            => Debug.WriteLine($"[WindowRemoved] Title='{args.Window?.Title}'");
 
         factory.WindowMoveDragBegin += (_, args) =>
         {
@@ -133,63 +103,40 @@ public class MainWindowViewModel : BindableBase
             Debug.WriteLine($"[WindowMoveDragBegin] Title='{args.Window?.Title}', Cancel={args.Cancel}, X='{args.Window?.X}', Y='{args.Window?.Y}'");
         };
 
-        factory.WindowMoveDrag += (_, args) =>
-        {
-            Debug.WriteLine($"[WindowMoveDrag] Title='{args.Window?.Title}', X='{args.Window?.X}', Y='{args.Window?.Y}");
-        };
+        factory.WindowMoveDrag += (_, args)
+            => Debug.WriteLine($"[WindowMoveDrag] Title='{args.Window?.Title}', X='{args.Window?.X}', Y='{args.Window?.Y}");
 
-        factory.WindowMoveDragEnd += (_, args) =>
-        {
-            Debug.WriteLine($"[WindowMoveDragEnd] Title='{args.Window?.Title}', X='{args.Window?.X}', Y='{args.Window?.Y}");
-        };
+        factory.WindowMoveDragEnd += (_, args)
+            => Debug.WriteLine($"[WindowMoveDragEnd] Title='{args.Window?.Title}', X='{args.Window?.X}', Y='{args.Window?.Y}");
 
-        factory.WindowActivated += (_, args) =>
-        {
-            Debug.WriteLine($"[WindowActivated] Title='{args.Window?.Title}'");
-        };
+        factory.WindowActivated += (_, args)
+            => Debug.WriteLine($"[WindowActivated] Title='{args.Window?.Title}'");
 
-        factory.DockableActivated += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableActivated] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockableActivated += (_, args)
+            => Debug.WriteLine($"[DockableActivated] Title='{args.Dockable?.Title}'");
 
-        factory.WindowDeactivated += (_, args) =>
-        {
-            Debug.WriteLine($"[WindowDeactivated] Title='{args.Window?.Title}'");
-        };
+        factory.WindowDeactivated += (_, args)
+            => Debug.WriteLine($"[WindowDeactivated] Title='{args.Window?.Title}'");
 
-        factory.DockableDeactivated += (_, args) =>
-        {
-            Debug.WriteLine($"[DockableDeactivated] Title='{args.Dockable?.Title}'");
-        };
+        factory.DockableDeactivated += (_, args)
+            => Debug.WriteLine($"[DockableDeactivated] Title='{args.Dockable?.Title}'");
     }
 
     public void CloseLayout()
     {
-        if (Layout is IDock dock)
-        {
-            if (dock.Close.CanExecute(null))
-            {
-                dock.Close.Execute(null);
-            }
-        }
+        if (Layout is IDock dock && dock.Close.CanExecute(null))
+            dock.Close.Execute(null);
     }
 
     public void ResetLayout()
     {
-        if (Layout is not null)
-        {
-            if (Layout.Close.CanExecute(null))
-            {
-                Layout.Close.Execute(null);
-            }
-        }
+        if (Layout is not null && Layout.Close.CanExecute(null))
+            Layout.Close.Execute(null);
 
         var layout = _factory?.CreateLayout();
         if (layout is not null)
-        {
             _factory?.InitLayout(layout);
-            Layout = layout;
-        }
+
+        Layout = layout;
     }
 }

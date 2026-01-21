b94a08b7125fbc843974afee8cb22b3f4bc516ce
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-01-21 10:35:33 +0100

    Inline Dock DI registrations in sample

---
+++ samples/DockReactiveUIDiSample/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+using Dock.Model;
+using Dock.Model.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DockReactiveUIDiSample;
+
+/// <summary>
+/// Provides DI registration helpers for the sample.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers Dock model services using the provided factory and serializer types.
+    /// </summary>
+    /// <typeparam name="TFactory">Implementation of <see cref="IFactory"/>.</typeparam>
+    /// <typeparam name="TSerializer">Implementation of <see cref="IDockSerializer"/>.</typeparam>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The service collection.</returns>
+    public static IServiceCollection AddDock<TFactory, TSerializer>(this IServiceCollection services)
+        where TFactory : class, IFactory
+        where TSerializer : class, IDockSerializer
+    {
+        services.AddSingleton<IDockState, DockState>();
+        services.AddSingleton<TFactory>();
+        services.AddSingleton<IFactory>(static sp => sp.GetRequiredService<TFactory>());
+        services.AddSingleton<TSerializer>();
+        services.AddSingleton<IDockSerializer>(static sp => sp.GetRequiredService<TSerializer>());
+        return services;
+    }
+}

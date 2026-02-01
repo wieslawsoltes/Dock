922d7e18a994def642e0543c7af67d0196021fbf
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-02-01 16:08:22 +0100

    Expose pinned display mode in sample options

--- samples/DockMvvmSample/Views/DockableOptionsView.axaml.cs
+++ samples/DockMvvmSample/Views/DockableOptionsView.axaml.cs
@@ -1,10 +1,18 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Dock.Model.Core;
 
 namespace DockMvvmSample.Views;
 
 public partial class DockableOptionsView : UserControl
 {
+    public static object?[] PinnedDockDisplayModeOptions { get; } =
+    {
+        null,
+        PinnedDockDisplayMode.Overlay,
+        PinnedDockDisplayMode.Inline
+    };
+
     public DockableOptionsView()
     {
         InitializeComponent();

1555c8e80e63d162b63c140c14e763f503359202
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-02-04 22:27:17 +0100

    Add tab strip detach leak tests

--- tests/Dock.Avalonia.LeakTests/DocumentTabStripLeakTests.cs
+++ tests/Dock.Avalonia.LeakTests/DocumentTabStripLeakTests.cs
@@ -65,7 +65,70 @@ public class DocumentTabStripLeakTests
         GC.KeepAlive(result.WindowKeepAlive);
     }
 
+    [ReleaseFact]
+    public void DocumentTabStripItem_DetachWhileWindowAlive_DoesNotLeak()
+    {
+        var result = RunInSession(() =>
+        {
+            var context = LeakContext.Create();
+            context.DocumentDock.CanCloseLastDockable = false;
+
+            var tabStrip = new DocumentTabStrip
+            {
+                DataContext = context.DocumentDock,
+                ItemsSource = context.DocumentDock.VisibleDockables
+            };
+
+            var window = new Window { Content = tabStrip };
+            window.Styles.Add(new FluentTheme());
+            window.Styles.Add(new DockFluentTheme());
+
+            ShowWindow(window);
+            tabStrip.ApplyTemplate();
+            tabStrip.UpdateLayout();
+            DrainDispatcher();
+
+            var tabItem = tabStrip.ContainerFromIndex(0) as DocumentTabStripItem
+                          ?? FindVisualDescendant<DocumentTabStripItem>(tabStrip);
+            if (tabItem is null)
+            {
+                throw new InvalidOperationException("DocumentTabStripItem was not generated.");
+            }
+
+            var startPoint = new Point(tabItem.Bounds.Width / 2, tabItem.Bounds.Height / 2);
+            var dragPoint = new Point(
+                startPoint.X + DockSettings.MinimumHorizontalDragDistance + 20,
+                startPoint.Y + DockSettings.MinimumVerticalDragDistance + 20);
+
+            RaisePointerPressed(tabItem, startPoint, MouseButton.Left);
+            RaisePointerMoved(tabItem, dragPoint, leftPressed: true);
+            DrainDispatcher();
+
+            context.DocumentDock.VisibleDockables!.Remove(context.Document);
+            DrainDispatcher();
+            ClearInputState(window);
+
+            var itemRef = new WeakReference(tabItem);
+            tabItem = null;
+
+            return new DocumentTabStripItemLeakResult(itemRef, window, tabStrip, context.Document, context.DocumentDock);
+        });
+
+        AssertCollected(result.ItemRef);
+        GC.KeepAlive(result.WindowKeepAlive);
+        GC.KeepAlive(result.TabStripKeepAlive);
+        GC.KeepAlive(result.DocumentKeepAlive);
+        GC.KeepAlive(result.DockKeepAlive);
+    }
+
     private sealed record DocumentTabStripLeakResult(
         WeakReference TabStripRef,
         Window WindowKeepAlive);
+
+    private sealed record DocumentTabStripItemLeakResult(
+        WeakReference ItemRef,
+        Window WindowKeepAlive,
+        DocumentTabStrip TabStripKeepAlive,
+        Dock.Model.Core.IDockable DocumentKeepAlive,
+        Dock.Model.Core.IDock DockKeepAlive);
 }
--- tests/Dock.Avalonia.LeakTests/ToolTabStripLeakTests.cs
+++ tests/Dock.Avalonia.LeakTests/ToolTabStripLeakTests.cs
@@ -1,8 +1,11 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Themes.Fluent;
 using Dock.Avalonia.Controls;
 using Dock.Avalonia.Themes.Fluent;
+using Dock.Settings;
 using Xunit;
 using static Dock.Avalonia.LeakTests.LeakTestHelpers;
 using static Dock.Avalonia.LeakTests.LeakTestSession;
@@ -53,8 +56,108 @@ public class ToolTabStripLeakTests
         GC.KeepAlive(result.DockableKeepAlive);
     }
 
+    [ReleaseFact]
+    public void ToolTabStripItem_DetachWhileWindowAlive_DoesNotLeak()
+    {
+        var result = RunInSession(() =>
+        {
+            var context = LeakContext.Create();
+
+            var tabStrip = new ToolTabStrip
+            {
+                DataContext = context.ToolDock
+            };
+
+            var tabItem = new ToolTabStripItem
+            {
+                DataContext = context.Tool
+            };
+            tabStrip.Items.Add(tabItem);
+
+            var window = new Window { Content = tabStrip };
+            window.Styles.Add(new FluentTheme());
+            window.Styles.Add(new DockFluentTheme());
+
+            ShowWindow(window);
+            tabStrip.ApplyTemplate();
+            tabStrip.UpdateLayout();
+            DrainDispatcher();
+
+            var startPoint = new Point(tabItem.Bounds.Width / 2, tabItem.Bounds.Height / 2);
+            var dragPoint = new Point(
+                startPoint.X + DockSettings.MinimumHorizontalDragDistance + 20,
+                startPoint.Y + DockSettings.MinimumVerticalDragDistance + 20);
+
+            RaisePointerPressed(tabItem, startPoint, MouseButton.Left);
+            RaisePointerMoved(tabItem, dragPoint, leftPressed: true);
+            DrainDispatcher();
+
+            tabStrip.Items.Remove(tabItem);
+            DrainDispatcher();
+            ClearInputState(window);
+
+            var itemRef = new WeakReference(tabItem);
+            tabItem = null;
+
+            return new ToolTabStripItemLeakResult(itemRef, window, tabStrip, context.Tool, context.ToolDock);
+        });
+
+        AssertCollected(result.ItemRef);
+        GC.KeepAlive(result.WindowKeepAlive);
+        GC.KeepAlive(result.TabStripKeepAlive);
+        GC.KeepAlive(result.ToolKeepAlive);
+        GC.KeepAlive(result.DockKeepAlive);
+    }
+
+    [ReleaseFact]
+    public void ToolTabStrip_DetachWhileWindowAlive_DoesNotLeak()
+    {
+        var result = RunInSession(() =>
+        {
+            var context = LeakContext.Create();
+
+            var tabStrip = new ToolTabStrip
+            {
+                ItemsSource = context.ToolDock.VisibleDockables
+            };
+
+            var window = new Window { Content = tabStrip };
+            window.Styles.Add(new FluentTheme());
+            window.Styles.Add(new DockFluentTheme());
+
+            ShowWindow(window);
+            tabStrip.ApplyTemplate();
+            tabStrip.UpdateLayout();
+            DrainDispatcher();
+
+            window.Content = new Border();
+            DrainDispatcher();
+
+            var tabStripRef = new WeakReference(tabStrip);
+            tabStrip = null;
+
+            return new ToolTabStripDetachLeakResult(tabStripRef, window, context.ToolDock);
+        });
+
+        AssertCollected(result.TabStripRef);
+        GC.KeepAlive(result.WindowKeepAlive);
+        GC.KeepAlive(result.DockKeepAlive);
+    }
+
     private sealed record ToolTabStripLeakResult(
         WeakReference TabStripRef,
         ToolTabStripItem ItemKeepAlive,
         object DockableKeepAlive);
+
+    private sealed record ToolTabStripItemLeakResult(
+        WeakReference ItemRef,
+        Window WindowKeepAlive,
+        ToolTabStrip TabStripKeepAlive,
+        Dock.Model.Core.IDockable ToolKeepAlive,
+        Dock.Model.Core.IDock DockKeepAlive);
+
+    private sealed record ToolTabStripDetachLeakResult(
+        WeakReference TabStripRef,
+        Window WindowKeepAlive,
+        Dock.Model.Core.IDock DockKeepAlive);
 }

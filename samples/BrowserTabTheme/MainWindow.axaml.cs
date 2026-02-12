b548ff1c8795c0298e893ca5bd4b6d5652afbd00
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-02-12 17:10:49 +0100

    Use XAML dock control in BrowserTabTheme main window
    (cherry picked from commit 22724cb4db9cd0b27aa1316ab93cdfbad1b373d5)

--- samples/BrowserTabTheme/MainWindow.axaml.cs
+++ samples/BrowserTabTheme/MainWindow.axaml.cs
@@ -7,9 +7,5 @@ public partial class MainWindow : Avalonia.Controls.Window
     public MainWindow()
     {
         AvaloniaXamlLoader.Load(this);
-        Title = "BrowserTabTheme Sample";
-        Width = 1300;
-        Height = 820;
     }
 }
-

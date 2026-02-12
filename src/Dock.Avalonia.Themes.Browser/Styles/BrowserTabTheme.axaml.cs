2894aa41a3e8c925fe5003b47c51647e34feb156
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-02-12 17:40:44 +0100

    Extract browser tab theme into reusable themes project

--- src/Dock.Avalonia.Themes.Browser/Styles/BrowserTabTheme.axaml.cs
+++ src/Dock.Avalonia.Themes.Browser/Styles/BrowserTabTheme.axaml.cs
@@ -6,13 +6,20 @@ using Avalonia.Markup.Xaml;
 using Avalonia.Styling;
 using Dock.Avalonia.Themes;
 
-namespace BrowserTabTheme.Themes;
+namespace Dock.Avalonia.Themes.Browser;
 
+/// <summary>
+/// Browser-style Dock theme overlay that can be applied on top of <c>DockFluentTheme</c>.
+/// </summary>
 public class BrowserTabTheme : Styles, IResourceNode
 {
     private readonly ResourceDictionary _compactStyles;
     private DockDensityStyle _densityStyle;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BrowserTabTheme"/> class.
+    /// </summary>
+    /// <param name="serviceProvider">Optional XAML service provider used during loading.</param>
     public BrowserTabTheme(IServiceProvider? serviceProvider = null)
     {
         AvaloniaXamlLoader.Load(serviceProvider, this);
@@ -26,18 +33,28 @@ public class BrowserTabTheme : Styles, IResourceNode
         }
     }
 
+    /// <summary>
+    /// Backing direct property for <see cref="DensityStyle"/>.
+    /// </summary>
     public static readonly DirectProperty<BrowserTabTheme, DockDensityStyle> DensityStyleProperty =
         AvaloniaProperty.RegisterDirect<BrowserTabTheme, DockDensityStyle>(
             nameof(DensityStyle),
             o => o.DensityStyle,
             (o, v) => o.DensityStyle = v);
 
+    /// <summary>
+    /// Gets or sets the density mode used by the browser theme resources.
+    /// </summary>
     public DockDensityStyle DensityStyle
     {
         get => _densityStyle;
         set => SetAndRaise(DensityStyleProperty, ref _densityStyle, value);
     }
 
+    /// <summary>
+    /// Notifies hosted resources when density changes so consumers can refresh theme resources.
+    /// </summary>
+    /// <param name="change">Property change information.</param>
     protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
     {
         base.OnPropertyChanged(change);

88f7d4e7b244cae2f3a291afb055ca5614166e3e
Dan Walmsley <4672627+danwalmsley@users.noreply.github.com>
2026-02-23 19:21:24 +0000

    allow tabstrip to be used independently of a documentcontrol

--- src/Dock.Avalonia/Controls/DocumentTabStrip.axaml.cs
+++ src/Dock.Avalonia/Controls/DocumentTabStrip.axaml.cs
@@ -80,6 +80,30 @@ public class DocumentTabStrip : TabStrip
         AvaloniaProperty.Register<DocumentTabStrip, ControlTheme?>(nameof(CreateButtonTheme));
 
     /// <summary>
+    /// Defines the <see cref="IconTemplate"/> property.
+    /// </summary>
+    public static readonly StyledProperty<object?> IconTemplateProperty =
+        AvaloniaProperty.Register<DocumentTabStrip, object?>(nameof(IconTemplate));
+
+    /// <summary>
+    /// Defines the <see cref="HeaderTemplate"/> property.
+    /// </summary>
+    public static readonly StyledProperty<IDataTemplate?> HeaderTemplateProperty =
+        AvaloniaProperty.Register<DocumentTabStrip, IDataTemplate?>(nameof(HeaderTemplate));
+
+    /// <summary>
+    /// Defines the <see cref="ModifiedTemplate"/> property.
+    /// </summary>
+    public static readonly StyledProperty<IDataTemplate?> ModifiedTemplateProperty =
+        AvaloniaProperty.Register<DocumentTabStrip, IDataTemplate?>(nameof(ModifiedTemplate));
+
+    /// <summary>
+    /// Defines the <see cref="CloseTemplate"/> property.
+    /// </summary>
+    public static readonly StyledProperty<IDataTemplate?> CloseTemplateProperty =
+        AvaloniaProperty.Register<DocumentTabStrip, IDataTemplate?>(nameof(CloseTemplate));
+
+    /// <summary>
     /// Defines the <see cref="CreateButtonTemplate"/> property.
     /// </summary>
     public static readonly StyledProperty<IDataTemplate?> CreateButtonTemplateProperty =
@@ -140,6 +164,42 @@ public class DocumentTabStrip : TabStrip
     {
         get => GetValue(CreateButtonThemeProperty);
         set => SetValue(CreateButtonThemeProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets tab icon template.
+    /// </summary>
+    public object? IconTemplate
+    {
+        get => GetValue(IconTemplateProperty);
+        set => SetValue(IconTemplateProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets tab header template.
+    /// </summary>
+    public IDataTemplate? HeaderTemplate
+    {
+        get => GetValue(HeaderTemplateProperty);
+        set => SetValue(HeaderTemplateProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets tab modified template.
+    /// </summary>
+    public IDataTemplate? ModifiedTemplate
+    {
+        get => GetValue(ModifiedTemplateProperty);
+        set => SetValue(ModifiedTemplateProperty, value);
+    }
+
+    /// <summary>
+    /// Gets or sets tab close template.
+    /// </summary>
+    public IDataTemplate? CloseTemplate
+    {
+        get => GetValue(CloseTemplateProperty);
+        set => SetValue(CloseTemplateProperty, value);
     }
 
     /// <summary>

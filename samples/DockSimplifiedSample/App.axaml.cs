edc54949e86e19f426727c11b0332277a3cf63ae
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-07-14 22:00:10 +0200

    Use observable layout collections in simplified sample

--- samples/DockSimplifiedSample/App.axaml.cs
+++ samples/DockSimplifiedSample/App.axaml.cs
@@ -1,12 +1,7 @@
-using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Dock.Avalonia.Controls;
-using Dock.Model.Avalonia;
-using Dock.Model.Avalonia.Controls;
-using Dock.Model.Controls;
-using Dock.Model.Core;
 
 namespace DockSimplifiedSample;
 
@@ -25,7 +20,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            var factory = new Factory(CreateLayout);
+            var factory = new DockFactory();
             var layout = factory.CreateLayout();
 
             var mainWindow = new MainWindow
@@ -43,53 +38,5 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
-    }
-    
-    private static IRootDock CreateLayout()
-    {
-        var document = new Document { Id = "Document1", Title = "Document 1" };
-        var tool = new Tool { Id = "Tool1", Title = "Tool 1" };
-        
-        var documentDock = new DocumentDock
-        {
-            Id = "Documents",
-            Title = "Documents",
-            VisibleDockables = new List<IDockable> { document },
-            ActiveDockable = document
-        };
-        
-        var toolDock = new ToolDock
-        {
-            Id = "Tools",
-            Title = "Tools",
-            VisibleDockables = new List<IDockable> { tool },
-            ActiveDockable = tool,
-            Alignment = Alignment.Left
-        };
-        
-        var mainLayout = new ProportionalDock
-        {
-            Id = "MainLayout",
-            Orientation = Orientation.Horizontal,
-            VisibleDockables = new List<IDockable> 
-            { 
-                toolDock, 
-                new ProportionalDockSplitter { CanResize = true }, 
-                documentDock 
-            }
-        };
-        
-        toolDock.Proportion = 0.25;
-        documentDock.Proportion = 0.75;
-        
-        var rootDock = new RootDock
-        {
-            Id = "Root",
-            Title = "Root",
-            VisibleDockables = new List<IDockable> { mainLayout },
-            ActiveDockable = mainLayout
-        };
-        
-        return rootDock;
     }
 }
